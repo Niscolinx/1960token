307d16d59a23c0209ab603470e488eadfd2160d3
Igboanugwo Collins <munisco12@gmail.com>
2022-05-30 15:20:01 +0100

    working on forms

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -55,7 +55,7 @@ const Register = () => {
                         className='block text-gray-700 text-sm font-bold mb-2'
                         htmlFor='password'
                     >
-                        Password
+                        Confirm Password
                     </label>
                     <input
                         className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
