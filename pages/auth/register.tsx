0a98bdb7b9130567764fab1a99df85a108c4ac69
Igboanugwo Collins <munisco12@gmail.com>
2022-05-30 15:40:53 +0100

    working on forms

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -39,7 +39,7 @@ const Register = () => {
                         Password
                     </label>
                     <input
-                        className='shadow appearance-none border border-red-500 rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+                        className='shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
                         id='password'
                         type='password'
                     />
@@ -55,7 +55,7 @@ const Register = () => {
                         Confirm Password
                     </label>
                     <input
-                        className='shadow appearance-none border border-red-500 rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+                        className='shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
                         id='confirmPassword'
                         type='password'
                     />
