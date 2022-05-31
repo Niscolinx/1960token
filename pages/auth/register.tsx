16ba108456001186516336bc12bcfc6763a5fd5f
Igboanugwo Collins <munisco12@gmail.com>
2022-05-31 14:31:07 +0100

    working on form errors

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -11,8 +11,8 @@ const Register = () => {
 
     const isValidMail = (e: string): Boolean => {
         console.log({ e })
-        const emailRegex = RegExp(
-            /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        const emailRegex = new RegExp(
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         )
 
         const isValid =  emailRegex.test(e)
