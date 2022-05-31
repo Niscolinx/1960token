3b422bcb01dea9bea8e1437b22e07870a26c81d8
Igboanugwo Collins <munisco12@gmail.com>
2022-05-31 16:20:46 +0100

    adder user model

---
+++ pages/api/models/User.ts
@@ -0,0 +1,61 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+        },
+
+        role: {
+            type: String,
+            default: 'Customer',
+            required: true,
+        },
+
+        status: {
+            type: String,
+            required: true,
+            default: 'Active',
+        },
+
+        password: {
+            type: String,
+            required: true,
+        },
+
+        pendingDeposits: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'pendingDeposit',
+            },
+        ],
+        pendingWithdrawals: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'pendingWithdrawal',
+            },
+        ],
+        totalDeposits: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'deposit',
+            },
+        ],
+        totalWithdrawals: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'withdraw',
+            },
+        ],
+    },
+    { timestamps: true }
+)
+
+module.exports = mongoose.model('users', userSchema)
