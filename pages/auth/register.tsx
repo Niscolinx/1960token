6ede8fd387e139a21ecd8223bbf3a8999a818056
Igboanugwo Collins <munisco12@gmail.com>
2022-05-31 11:51:11 +0100

    working on the form

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -121,7 +121,7 @@ const Register = () => {
                         Confirm Password
                     </label>
                     <input
-                        className='shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+                        className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline ${ error ? 'border-red-500' : ''}`}
                         id='confirmPassword'
                         name='confirmPassword'
                         type='password'
