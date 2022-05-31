00a06b762fe82851a547fb2aee0567de73a2df95
Igboanugwo Collins <munisco12@gmail.com>
2022-05-31 16:08:18 +0100

    validating password

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -23,11 +23,6 @@ const Register = () => {
         }
     }
 
-    // useEffect(() => {
-    //     console.log('call message handler')
-    //     messageHandler()
-    // }, [error, errorFields])
-
     const isValidMail = (e: string): Boolean => {
         const emailRegex = new RegExp(
             /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
