a1e0593f35eec1beaf04ad9dea1bd3506e6854ae
Igboanugwo Collins <munisco12@gmail.com>
2022-05-31 18:47:01 +0100

    working on client side  register auth

---
+++ pages/_middleware.ts
@@ -0,0 +1,6 @@
+import type { NextFetchEvent, NextRequest } from 'next/server'
+import dbConnect from '../lib/dbConnect'
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+    await dbConnect()
+}
