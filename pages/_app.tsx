252161770d5ff9421341845625eab82a773eafb0
Igboanugwo Collins <munisco12@gmail.com>
2022-06-01 14:03:47 +0100

    added next auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,35 +2,36 @@ import 'windi.css'
 import Head from 'next/head'
 import Link from 'next/link'
 import { AppProps } from 'next/app'
+import {SessionProvider} from 'next-auth/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  console.log('_app running')
-  return (
-    <>
-      <Head>
-        <title>1960Token</title>
-      </Head>
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+    console.log('_app running')
+    return (
+        <>
+            <Head>
+                <title>1960Token</title>
+            </Head>
 
-      <div className="top-bar">
-        <div className="nav">
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-          <Link href="/auth/register">
-            <a>Register</a>
-          </Link>
-          <Link href="/auth/login">
-            <a>Login</a>
-          </Link>
-        </div>
-
-     
-      </div>
-      <div className="grid wrapper">
-        <Component {...pageProps} />
-      </div>
-    </>
-  )
+            <div className='top-bar'>
+                <div className='nav'>
+                    <Link href='/'>
+                        <a>Home</a>
+                    </Link>
+                    <Link href='/auth/register'>
+                        <a>Register</a>
+                    </Link>
+                    <Link href='/auth/login'>
+                        <a>Login</a>
+                    </Link>
+                </div>
+            </div>
+            <div className='grid wrapper'>
+                <SessionProvider session={session} refetchInterval={5 * 60}>
+                    <Component {...pageProps} />
+                </SessionProvider>{' '}
+            </div>
+        </>
+    )
 }
 
 export default MyApp
