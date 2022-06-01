014be698a8720f72f74854c176307ce7b394dd2a
Igboanugwo Collins <munisco12@gmail.com>
2022-06-01 12:45:39 +0100

    merged dev

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { json } from 'stream/consumers'
 import User from '../../../models/User'
 
 export default async function login(
@@ -10,8 +11,15 @@ export default async function login(
     try{
         const users = await User.find()
         console.log({users})
+
+        res.status(200).json(
+           'users found'
+        )
     }
     catch(err){
         console.log({err})
+        res.status(400).json(
+            'error'
+        )
     }
 }
