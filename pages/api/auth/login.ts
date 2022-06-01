25355984cdf8b3a2d37af533b0ae4322c5ed630d
Igboanugwo Collins <munisco12@gmail.com>
2022-06-01 15:08:46 +0100

    added next auth

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,38 +1,31 @@
-import { IUser } from './../../../models/User';
-import  bcrypt  from 'bcryptjs';
+import { IUser } from './../../../models/User'
+import bcrypt from 'bcryptjs'
 import { NextApiRequest, NextApiResponse } from 'next'
 import User from '../../../models/User'
 
-export default async function login(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
+export default async function login(req: NextApiRequest, res: NextApiResponse) {
     console.log('login.......', req.body)
 
-    const {email, password} = req.body
+    const { email, password } = req.body
 
-    try{
-        const user: IUser | null = await User.findOne({email})
+    try {
+        const user: IUser | null = await User.findOne({ email })
 
         console.log(user)
-        if(!user){
+        if (!user) {
             res.status(400).json('invalid user')
         }
 
-
         const checkPassword = await bcrypt.compare(password, user!.password)
+        if (!checkPassword) {
+            res.status(401).json('Incorrect password')
+        }
 
-        console.log({checkPassword})
-        res.status(200).json(
-           {
-               user
-           }
-        )
-    }
-    catch(err){
-        console.log({err})
-        res.status(400).json(
-            'error'
-        )
+        res.status(200).json({
+            user,
+        })
+    } catch (err) {
+        console.log({ err })
+        res.status(400).json('error')
     }
 }
