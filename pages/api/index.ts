6deef7a6b9e628d9cf2b68dc847f6d5be6f0f762
Igboanugwo Collins <munisco12@gmail.com>
2022-06-03 09:16:24 +0100

    added jwt env

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,6 +1,8 @@
-import type { NextFetchEvent, NextRequest } from 'next/server'
+import { NextApiResponse } from 'next'
+import type { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 
-export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+export default async function Home(req: NextRequest, res: NextApiResponse) {
     console.log('running from index')
+    res.json('hello')
     //await dbConnect()
 }
