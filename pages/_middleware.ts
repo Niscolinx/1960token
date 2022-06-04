3001d388b44410f4cb26f8afcaef036a6c1e8564
Igboanugwo Collins <munisco12@gmail.com>
2022-06-04 07:30:01 +0100

    added jwt env

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,5 +3,6 @@ import dbConnect from '../lib/dbConnect'
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
     console.log("running")
+    console.log(req.cookies)
     //await dbConnect()
 }
