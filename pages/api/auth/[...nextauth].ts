b42012989c9c7731fafe7929fa3b4f8cfa9f40f1
Igboanugwo Collins <munisco12@gmail.com>
2022-06-06 15:32:52 +0100

    working on next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -20,6 +20,7 @@ export default NextAuth({
                 password: { label: 'Password', type: 'password' },
             },
             async authorize(credentials, req) {
+                console.log({req, credentials})
                 // You need to provide your own logic here that takes the credentials
                 // submitted and returns either a object representing a user or value
                 // that is false/null if the credentials are invalid.
