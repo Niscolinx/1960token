6f9ff33b0bc2280ad39ecb72232a6599a6939fbc
Igboanugwo Collins <munisco12@gmail.com>
2022-06-06 15:19:54 +0100

    working on next auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,8 +12,8 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
                 <title>1960Token</title>
             </Head>
 
-            <div className='flex w-full'>
-                <div className='w-full'>
+            <div className='flex w-1/3'>
+                <div className='w-full flex justify-between'>
                     <Link href='/'>
                         <a>Home</a>
                     </Link>
