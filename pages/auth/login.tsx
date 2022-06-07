a12c186bdf9fe1ea9a77a940622ea2fcc30eab72
Igboanugwo Collins <munisco12@gmail.com>
2022-06-07 22:02:30 +0100

    working on login auth

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -112,6 +112,7 @@ const Login = (props:any) => {
                     >
                         Email
                     </label>
+                    <input type="hidden" name='csrfToken' />
                     <input
                         className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline ${
                             error && errorFields.includes('email')
