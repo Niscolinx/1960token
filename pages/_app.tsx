aa5b598390585cab2529d9ba51fee5a00a34861f
Igboanugwo Collins <munisco12@gmail.com>
2022-06-08 04:48:53 +0100

    working on login auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { AppProps } from 'next/app'
 import {SessionProvider} from 'next-auth/react'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-    console.log('_app running')
+    console.log('_app running', session)
     return (
         <>
             <Head>
