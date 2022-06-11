033223333afb9347de7d8c5210f334db90c1d758
Igboanugwo Collins <munisco12@gmail.com>
2022-06-11 21:46:14 +0100

    using next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,42 +12,42 @@ import clientPromise from '../../../lib/mongodb'
 
 dbConnect()
 export default NextAuth({
-    adapter: MongoDBAdapter(clientPromise),
+   // adapter: MongoDBAdapter(clientPromise),
 
     providers: [
-        CredentialsProvider({
-            // The name to display on the sign in form (e.g. 'Sign in with...')
-            name: 'Credentials',
-            // The credentials is used to generate a suitable form on the sign in page.
-            // You can specify whatever fields you are expecting to be submitted.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
-            credentials: {
-                email: {
-                    label: 'Email',
-                    type: 'email',
-                },
-                password: { label: 'Password', type: 'password' },
-            },
-            async authorize(credentials, req) {
-                const email = credentials?.email
-                const password = credentials?.password
+        // CredentialsProvider({
+        //     // The name to display on the sign in form (e.g. 'Sign in with...')
+        //     name: 'Credentials',
+        //     // The credentials is used to generate a suitable form on the sign in page.
+        //     // You can specify whatever fields you are expecting to be submitted.
+        //     // e.g. domain, username, password, 2FA token, etc.
+        //     // You can pass any HTML attribute to the <input> tag through the object.
+        //     credentials: {
+        //         email: {
+        //             label: 'Email',
+        //             type: 'email',
+        //         },
+        //         password: { label: 'Password', type: 'password' },
+        //     },
+        //     async authorize(credentials, req) {
+        //         const email = credentials?.email
+        //         const password = credentials?.password
 
-                return axios
-                    .post(`${serverUrl}/api/auth/login`, {
-                        email,
-                        password,
-                    })
-                    .then(({ data }) => {
-                        console.log({ data })
-                        return data
-                    })
-                    .catch((err) => {
-                        console.log({ err })
-                        return 
-                    })
-            },
-        }),
+        //         return axios
+        //             .post(`${serverUrl}/api/auth/login`, {
+        //                 email,
+        //                 password,
+        //             })
+        //             .then(({ data }) => {
+        //                 console.log({ data })
+        //                 return data
+        //             })
+        //             .catch((err) => {
+        //                 console.log({ err })
+        //                 return 
+        //             })
+        //     },
+        // }),
         // EmailProvider({
         //     server: process.env.EMAIL_SERVER,
         //     from: process.env.EMAIL_FROM,
@@ -64,22 +64,22 @@ export default NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
         }),
     ],
-    pages: {
-        signIn: '/auth/login',
-    },
-    callbacks: {
-        async jwt({ user, token }) {
-            return Object.assign(token, user)
-            //return token
-        },
-        async session({ session, token }) {
-           return Object.assign(session, token)
-           //return session
-        },
-    },
-    theme: {
-        colorScheme: 'dark',
-    },
+    // pages: {
+    //     signIn: '/auth/login',
+    // },
+//    callbacks: {
+//     jwt: async ({ token, user }) => {
+//         user && (token.user = user)
+//         return token
+//     },
+//     session: async ({ session, token }) => {
+//         session.user = token
+//         return session
+//     }
+// },
+//     theme: {
+//         colorScheme: 'dark',
+//     },
 
-    secret: process.env.JWT_SECRET,
+//     secret: process.env.JWT_SECRET,
 })
