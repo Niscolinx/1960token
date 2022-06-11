52cf7778092cf6533160de65732102617da0262c
Igboanugwo Collins <munisco12@gmail.com>
2022-06-11 11:58:55 +0100

    working on email sign up

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -43,8 +43,15 @@ export default NextAuth({
             },
         }),
         EmailProvider({
-            server: process.env.EMAIL_SERVER || '',
-            from: process.env.EMAIL_FROM || '',
+            server: {
+                host: process.env.EMAIL_SERVER_HOST,
+                port: process.env.EMAIL_SERVER_PORT,
+                auth: {
+                    user: process.env.EMAIL_SERVER_USER,
+                    pass: process.env.EMAIL_SERVER_PASSWORD,
+                },
+            },
+            from: process.env.EMAIL_FROM,
         }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || '',
