7ba68ff1efeb2b1fa2ac522530a18a15a2cd37e0
Igboanugwo Collins <munisco12@gmail.com>
2022-06-11 19:45:48 +0100

    working on credentials

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -117,7 +117,7 @@ const Login = ({ providers }: LoginProps) => {
             <div className='w-full md:w-1/3 mx-auto'>
                 <form
                     id='login'
-                    className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-10'
+                    className='bg-white rounded px-8 pt-6 pb-8 mb-4 mt-10'
                     onSubmit={handleSubmit}
                 >
                     <p
@@ -171,7 +171,7 @@ const Login = ({ providers }: LoginProps) => {
                         />
                     </div>
 
-                    <div className='grid justify-center gap-2  md:gap-0 md:flex items-center md:justify-between'>
+                    <div className='grid justify-center gap-2  md:gap-0 md:flex items-center'>
                         <button
                             className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline'
                             type='submit'
@@ -200,7 +200,7 @@ const Login = ({ providers }: LoginProps) => {
                         </div>
                     )
                 })}
-                <div className='grid'>
+                <div className='grid mt-10 gap-2 px-8'>
                     <a
                         className='inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800'
                         href='#'
