49701800c43c942a5b0e3f31d4a4253090a61d4f
Igboanugwo Collins <munisco12@gmail.com>
2022-06-12 16:38:54 +0100

    displaying message on log in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,8 +12,8 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
                 <title>1960Token</title>
             </Head>
 
-            <div className='flex w-1/3'>
-                <div className='w-full flex justify-between'>
+            <div className='flex w-full md:w-1/3'>
+                <div className='w-full flex justify-between px-10 py-2'>
                     <Link href='/'>
                         <a>Home</a>
                     </Link>
