b25eb477623e3e8faf1050595cd7f8af5bc25032
Igboanugwo Collins <munisco12@gmail.com>
2022-06-13 12:31:25 +0100

    working on redirect and getting user data

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ const Index = () => {
    const { data: session } = useSession()
    console.log({ session })
 
+
   let isLoggedInMessage:string = ''
   if(session){
     isLoggedInMessage = `You are logged in as ${session.user?.name}`
@@ -17,20 +18,18 @@ const Index = () => {
 }
 
 export async function getServerSideProps() {
-  console.log("started connection.......")
 
     try {
         await dbConnect()
-      console.log('connected!!')
         return {
             props: { isConnected: 'true' },
         }
     } catch (e) {
-        console.error(e)
         return {
             props: { isConnected: 'false' },
         }
     }
 }
+
 
 export default Index
