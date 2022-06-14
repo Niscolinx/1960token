4467c6ab4f3745eca472f2be4881412a47b2139c
Igboanugwo Collins <munisco12@gmail.com>
2022-06-14 15:43:20 +0100

    working on protected routes

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -5,4 +5,6 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
     const {cookies} = req
 
     console.log('the cookies', cookies['next-auth.session-token'])
+
+    console.log("req", req)
 }
