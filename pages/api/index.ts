7d0cb36d38c747e3ecb99ad4e887d67f752b6bd0
Igboanugwo Collins <munisco12@gmail.com>
2022-06-14 18:20:29 +0100

    working on protected routes

--- pages/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiResponse } from 'next'
-import type { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
-
-export default async function Home(req: NextRequest, res: NextApiResponse) {
-    console.log('running from index')
-   
-    res.json('hello')
-    //await dbConnect()
-}
