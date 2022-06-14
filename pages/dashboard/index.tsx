34d11673fdfbb83d77f52ce1c51f95da9a637214
Igboanugwo Collins <munisco12@gmail.com>
2022-06-14 18:21:13 +0100

    working on protected routes

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,8 +1,17 @@
+import { useSession } from "next-auth/react"
+
 const Home = (props:any) => {
-    console.log({props})
-    return (
-        <div>The protected route</div>
-    )
+     const { data: session } = useSession()
+
+     console.log({ session })
+
+     let isLoggedInMessage: string = ''
+     if (session) {
+         isLoggedInMessage = `You are logged in as ${session.user?.name}`
+     } else {
+         isLoggedInMessage = `Not logged in`
+     }
+     return <div>{isLoggedInMessage}</div> 
 }
 
 export default Home
