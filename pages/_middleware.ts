1a8a6e7d55b28fc7ee32261594e6d3ee0f8a0d9b
Igboanugwo Collins <munisco12@gmail.com>
2022-06-14 15:37:26 +0100

    working on protected routes

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,4 @@
 import type { NextFetchEvent, NextRequest } from 'next/server'
-import dbConnect from '../lib/dbConnect'
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
  
