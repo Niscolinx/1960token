70c72a9ef17da54ff766a730877fbbe73d471596
Igboanugwo Collins <munisco12@gmail.com>
2022-06-14 15:52:12 +0100

    working on protected routes

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,10 +1,16 @@
+import { redirect } from 'next/dist/server/api-utils'
 import type { NextFetchEvent, NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
  
     const {cookies} = req
 
-     const sessionToken = cookies['next-auth.session-token'
+     const sessionToken = cookies['next-auth.session-token']
 
-    if()
+     let auth:Boolean = false
+
+     sessionToken ? auth = true : auth = false
+
+     console.log('the auth', {auth})
+   
 }
