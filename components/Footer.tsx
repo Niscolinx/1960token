b7b089c47d312e35a65400f0bc00c44d8c26cc4e
Igboanugwo Collins <munisco12@gmail.com>
2022-06-15 13:29:11 +0100

    added icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,37 +1,39 @@
 import Link from 'next/link'
 import React from 'react'
-import {CgProfile} from 'react-icons/cg'
-import {RiHome5Line} from 'react-icons/ri'
-import {GiTrade} from 'react-icons/gi'
-import {MdOutlineOndemandVideo} from 'react-icons/md'
+import { CgProfile } from 'react-icons/cg'
+import { RiHome5Line } from 'react-icons/ri'
+import { GiTrade } from 'react-icons/gi'
+import { MdOutlineOndemandVideo } from 'react-icons/md'
 
-const Footer =() => {
-  return <div className='bg-[#1a1a2d] text-[#ccccd0] flex justify-center'>
-      <Link href='/'>
-        <div className='grid justify-items-center'>
-            <RiHome5Line className='text-white'/>
-            <p>Home</p>
+const Footer = () => {
+    return (
+        <div className='bg-[#1a1a2d] text-[#ccccd0] flex justify-between'>
+            <Link href='/'>
+                <div className='grid justify-items-center'>
+                    <RiHome5Line className='text-white' />
+                    <p>Home</p>
+                </div>
+            </Link>
+            <Link href='/'>
+                <div className='grid justify-items-center'>
+                    <MdOutlineOndemandVideo className='text-blue-400' />
+                    <p>Ad</p>
+                </div>
+            </Link>
+            <Link href='/'>
+                <div className='grid justify-items-center'>
+                    <GiTrade />
+                    <p>Mine</p>
+                </div>
+            </Link>
+            <Link href='/'>
+                <div className='grid justify-items-center'>
+                    <CgProfile />
+                    <p>profile</p>
+                </div>
+            </Link>
         </div>
-      </Link>
-      <Link href='/'>
-        <div className='grid justify-items-center'>
-            <MdOutlineOndemandVideo className='text-blue-400'/>
-            <p>Ad</p>
-        </div>
-      </Link>
-      <Link href='/'>
-        <div className='grid justify-items-center'>
-            <GiTrade/>
-            <p>Mine</p>
-        </div>
-      </Link>
-      <Link href='/'>
-        <div className='grid justify-items-center'>
-            <CgProfile/>
-            <p>profile</p>
-        </div>
-      </Link>
-  </div>
+    )
 }
 
 export default Footer
