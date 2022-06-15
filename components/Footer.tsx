9d373310546b7f3843b52ee3f07e9688a289043f
Igboanugwo Collins <munisco12@gmail.com>
2022-06-15 13:47:22 +0100

    added icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,22 +10,38 @@ const Footer = () => {
         <div className='bg-[#1a1a2d] text-[#ccccd0] flex justify-between mt-8'>
             <Link href='/'>
                 <div className='grid justify-items-center'>
-                    <RiHome5Line className='text-orange-300 text-lg' />
+                    <RiHome5Line className='text-orange-300 text-2xl shadow'  style={{
+                        background: '#1a1a2d',
+                        boxShadow: `9px 9px 14px #12121f,
+             -9px -9px 14px #22223b`,
+                    }} />
                 </div>
             </Link>
             <Link href='/'>
                 <div className='grid justify-items-center'>
-                    <MdOutlineOndemandVideo className='text-orange-300 text-lg' />
+                    <MdOutlineOndemandVideo className='text-orange-300 text-2xl shadow'  style={{
+                        background: '#1a1a2d',
+                        boxShadow: `9px 9px 14px #12121f,
+             -9px -9px 14px #22223b`,
+                    }} />
                 </div>
             </Link>
             <Link href='/'>
                 <div className='grid justify-items-center'>
-                    <GiTrade className='text-orange-300 text-lg'/>
+                    <GiTrade className='text-orange-300 text-2xl shadow'  style={{
+                        background: '#1a1a2d',
+                        boxShadow: `9px 9px 14px #12121f,
+             -9px -9px 14px #22223b`,
+                    }}/>
                 </div>
             </Link>
             <Link href='/'>
                 <div className='grid justify-items-center'>
-                    <CgProfile className='text-orange-300 text-lg'/>
+                    <CgProfile className='text-orange-300 text-2xl shadow'  style={{
+                        background: '#1a1a2d',
+                        boxShadow: `9px 9px 14px #12121f,
+             -9px -9px 14px #22223b`,
+                    }}/>
                 </div>
             </Link>
         </div>
