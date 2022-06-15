3ad6ddd8ffd33cf4737782e1669b89912ce4d20c
Igboanugwo Collins <munisco12@gmail.com>
2022-06-15 22:03:24 +0100

    added countdown timer

--- components/countDownTimer.tsx
+++ components/countDownTimer.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import Countdown, { zeroPad } from 'react-countdown'
 
 function CountDownTimer() {
-
     interface IcountDown {
-        hours: number,
-        minutes: number,
-        seconds: number,
+        hours: number
+        minutes: number
+        seconds: number
         completed: Boolean
     }
 
@@ -20,17 +19,34 @@ function CountDownTimer() {
         } else {
             // Render a countdown
             return (
-                <span>
-                    {zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}{' '}
-                </span>
+                <div
+                    className=' justify-center grid w-max text-center py-2 px-10 rounded-lg place-self-center mt-10'
+                    style={{
+                        background: `linear-gradient(145deg, #1c1c30, #171729)`,
+                        boxShadow: `7px 7px 14px #161625,
+             -7px -7px 14px #1e1e35`,
+                        borderRadius: '50px',
+                    }}
+                >
+                    <p>
+                        Remaining mining time -{' '}
+                        <span>
+                            {zeroPad(hours)}:{zeroPad(minutes)}:
+                            {zeroPad(seconds)}
+                        </span>
+                    </p>
+                </div>
             )
         }
     }
-  return (
-      <div>
-          <Countdown date={Date.now() + (3600 * 1000) * 6} renderer={renderer} />
-      </div>
-  )
+    return (
+        <div>
+            <Countdown
+                date={Date.now() + 3600 * 1000 * 6}
+                renderer={renderer}
+            />
+        </div>
+    )
 }
 
 export default CountDownTimer
