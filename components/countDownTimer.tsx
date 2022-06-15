ea7be124d4edb79bcbcaa63e4a38d3b7db5f07d7
Igboanugwo Collins <munisco12@gmail.com>
2022-06-15 21:47:01 +0100

    added countdown timer

--- components/countDownTimer.tsx
+++ components/countDownTimer.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
-import Countdown from 'react-countdown'
+import Countdown, { zeroPad } from 'react-countdown'
 
 function CountDownTimer() {
+
+    interface IcountDown {
+        hours: number,
+        minutes: number,
+        seconds: number,
+        completed: Boolean
+    }
+
+    const Completionist = () => <span>You are good to go!</span>
+
+    // Renderer callback with condition
+    const renderer = ({ hours, minutes, seconds, completed }: IcountDown) => {
+        if (completed) {
+            // Render a completed state
+            return <Completionist />
+        } else {
+            // Render a countdown
+            return (
+                <span>
+                    {zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}{' '}
+                </span>
+            )
+        }
+    }
   return (
       <div>
-          <Countdown date={Date.now() + 10000} />
+          <Countdown date={Date.now() + 10000} renderer={renderer} />
       </div>
   )
 }
