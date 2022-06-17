554f11d73d203ca48d7a73ba383f81a14103e1e9
Igboanugwo Collins <munisco12@gmail.com>
2022-06-17 20:15:32 +0100

    added nav

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ const Footer = () => {
                     <GiTrade className='text-orange-300 text-2xl'  />
                 </div>
             </Link>
-            <Link href='/'>
+            <Link href='/dashboard'>
                 <div className='grid justify-items-center p-2 rounded-full cursor-pointer' style={{
                         background: `linear-gradient(145deg, #1c1c30, #171729)`,
                         boxShadow: ` 5px 5px 16px #0c0c15,
