4445a3b954038c8aae44b456e7ee8ac7be449296
Igboanugwo Collins <munisco12@gmail.com>
2022-06-17 16:21:55 +0100

    added png images instead of svg because of the file sizes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Footer from './Footer'
+import Nav from './nav'
 
 const Layout:React.FC<{}> = ({children}) => {
   return (
-      <div className='py-2 px-4 bg-[#1a1a2d] text-[#ccccd0]'>
+    <div className='py-2 px-4 bg-[#1a1a2d] text-[#ccccd0]'>
+        <Nav/>
           {children}
           <Footer />
       </div>
