a1f1e4b6be96f3bed441d539f18b2125633c5a22
Igboanugwo Collins <munisco12@gmail.com>
2022-06-17 18:54:48 +0100

    added png images instead of svg because of the file sizes

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 
 function nav() {
   return (
-   <nav>
-    <Link href='/api/auth/signin'>
-        Sign in
-    </Link>
-   </nav>
+      <nav>
+          <Link href='/api/auth/signin'>
+              <button className='bg-orange-300 text-[#1a1a2d] rounded px-2 py-1'>
+                  Sign in
+              </button>
+          </Link>
+      </nav>
   )
 }
 
