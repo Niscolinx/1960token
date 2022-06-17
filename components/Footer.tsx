75ae9b5e102b044083b8835d16895dc865e99983
Igboanugwo Collins <munisco12@gmail.com>
2022-06-17 16:28:41 +0100

    added nav

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ import { MdOutlineOndemandVideo } from 'react-icons/md'
 
 const Footer = () => {
     return (
-        <div className='bg-[#1a1a2d] text-[#ccccd0] flex justify-between mt-12 max-w-4xl mx-auto pb-2'>
+        <div className='bg-[#1a1a2d] text-[#ccccd0] flex justify-between mt-12 max-w-3xl mx-auto pb-2'>
             <Link href='/'>
                 <div className='grid justify-items-center p-2 rounded-full cursor-pointer' style={{
                         background: `linear-gradient(145deg, #1c1c30, #171729)`,
