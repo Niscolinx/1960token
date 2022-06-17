dea4ea9f7f79b8a1d95c9ccc08d601a820aebbe2
Igboanugwo Collins <munisco12@gmail.com>
2022-06-17 14:30:40 +0100

    resized svg carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,17 +49,17 @@ const Index = () => {
                 >
                     <SwiperSlide>
                         <div className='w-full md:w-2/3 rounded-4xl overflow-hidden'>
-                            <Image src='/carousel1.png' alt='' width='100%' height='100%' />
+                            <Image src='/carousel1.png' alt='' width='1920px' height='1080px' />
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
                         <div className='w-full md:w-2/3 rounded-4xl overflow-hidden'>
-                            <Image src='/carousel2.png' alt='' width='100%' height='100%' />
+                            <Image src='/carousel2.png' alt='' width='1920px' height='1080px' />
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
                         <div className='w-full md:w-2/3 rounded-4xl overflow-hidden'>
-                            <Image src='/carousel3.png' alt='' width='100%' height='100%' />
+                            <Image src='/carousel3.png' alt='' width='1920px' height='1080px' />
                         </div>
                     </SwiperSlide>
                 </Swiper>
