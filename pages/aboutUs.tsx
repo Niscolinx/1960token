302e194cc40a5ef119f79eed86aa80a39a46e2e5
Igboanugwo Collins <munisco12@gmail.com>
2022-06-18 20:31:06 +0100

    implemented mining time

--- pages/aboutUs.tsx
+++ pages/aboutUs.tsx
@@ -3,27 +3,87 @@ import React from 'react'
 function aboutUs() {
   return (
       <div>
-          <h2>About Us</h2>
-          <p>
-              1960Token is one of the most superior Mining and Affiliate
-              platform, offering Mining and Affiliate solutions for active day
-              Miner and Affiliate as well as client that are new to the
-              Platform. 1960Token offers its clients the best Mining and
-              Affiliate platforms, with superior connectivity. 1960Token is
-              revolutionizing online Mining and Affiliate.
-          </p>
+          <div className='grid'>
+              <h2>About Us</h2>
+              <p>
+                  1960Token is one of the most superior Mining and Affiliate
+                  platform, offering Mining and Affiliate solutions for active
+                  day Miner and Affiliate as well as client that are new to the
+                  Platform. 1960Token offers its clients the best Mining and
+                  Affiliate platforms, with superior connectivity. 1960Token is
+                  revolutionizing online Mining and Affiliate.
+              </p>
+          </div>
+          <div className='grid'>
+              <h3>Core Values</h3>
+              <ol>
+                  <li>Integrity and Trust</li>
+                  <li>Honesty and Fairness</li>
+                  <li>Transparency</li>
+                  <li>Commitments and Responsibility</li>
+                  <li>Reliability </li>
+                  <li>Flexibility</li>
+                  <li>Constant Innovation</li>
+                  <li>Strong Corporate Governance</li>
+              </ol>
+          </div>
 
-          <h3>Core Values</h3>
-          <ol>
-              <li>Integrity and Trust</li>
-              <li>Honesty and Fairness</li>
-              <li>Transparency</li>
-              <li>Commitments and Responsibility</li>
-              <li>Reliability </li>
-              <li>Flexibility</li>
-              <li>Constant Innovation</li>
-              <li>Strong Corporate Governance</li>
-          </ol>
+          <div className='grid'>
+              <h2>Confidentiality </h2>
+              <p>
+                  1960Token maintains appropriate controls and monitors the
+                  transfer of confidential and sensitive client information
+                  within and outside the Company on a need-to-know basis.
+                  1960Token does not deliberately disclose client information
+                  unless this is upon request of the client, or as required to
+                  do so by law.
+              </p>
+          </div>
+          <div className='grid'>
+              <h2>Why Us </h2>
+              <div className='grid'>
+                  <p>
+                      1960Token has grown to become one of the largest Mining
+                      and Affiliate Platform in the world by giving her clients
+                      what they really want. These are:
+                  </p>
+                  <ul>
+                      <li>
+                          FAST PAYMENT: You can make your withdrawal anytime you
+                          want as an Affiliate and as for Miners you withdraw
+                          only once in a month. You will receive your money on
+                          your crypto wallet within 24/hrs.{' '}
+                      </li>
+                      <li>
+                          SUPERIOR TECHNOLOGY: 1960Token has partnered with the
+                          world best mining technology companies to bring you
+                          the ultimate Mining and Affiliate experience
+                      </li>
+                  </ul>
+              </div>
+          </div>
+          <div className='grid'>
+              <h2>Confidentiality </h2>
+              <p>
+                  1960Token maintains appropriate controls and monitors the
+                  transfer of confidential and sensitive client information
+                  within and outside the Company on a need-to-know basis.
+                  1960Token does not deliberately disclose client information
+                  unless this is upon request of the client, or as required to
+                  do so by law.
+              </p>
+          </div>
+          <div className='grid'>
+              <h2>Confidentiality </h2>
+              <p>
+                  1960Token maintains appropriate controls and monitors the
+                  transfer of confidential and sensitive client information
+                  within and outside the Company on a need-to-know basis.
+                  1960Token does not deliberately disclose client information
+                  unless this is upon request of the client, or as required to
+                  do so by law.
+              </p>
+          </div>
       </div>
   )
 }
