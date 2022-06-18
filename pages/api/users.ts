9d5297ed17a485a8f1f6debd948212287af2aa06
Igboanugwo Collins <munisco12@gmail.com>
2022-06-18 14:16:19 +0100

    implemented mining time

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -2,10 +2,12 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import User from '../../models/User';
 
 export default async function users(req:NextApiRequest, res:NextApiResponse) {
+    console.log(req.body)
+
+    const {user} = req.body
 
     const users = await User.find()
     console.log({users})
     
-    console.log(req.body)
 }
 
