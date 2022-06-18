5b522b2aaa09a0eac39a965f069d4f6136fe0d70
Igboanugwo Collins <munisco12@gmail.com>
2022-06-18 20:26:13 +0100

    implemented mining time

---
+++ pages/aboutUs.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function aboutUs() {
+  return (
+      <div>
+          <h2>About Us</h2>
+          <p>
+              1960Token is one of the most superior Mining and Affiliate
+              platform, offering Mining and Affiliate solutions for active day
+              Miner and Affiliate as well as client that are new to the
+              Platform. 1960Token offers its clients the best Mining and
+              Affiliate platforms, with superior connectivity. 1960Token is
+              revolutionizing online Mining and Affiliate.
+          </p>
+
+          <h3>Core Values</h3>
+          <ol>
+              <li>Integrity and Trust</li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+          </ol>
+      </div>
+  )
+}
+
+export default aboutUs
