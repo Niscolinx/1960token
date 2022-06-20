854fead25fd73926f2b6884b6829e737473391a6
Igboanugwo Collins <munisco12@gmail.com>
2022-06-20 23:16:14 +0100

    working on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ const Index = () => {
 
     return (
         <>
-            <div className='grid gap-5 max-w-3xl mx-auto text-gray-400'>
+            <div className='grid gap-5 max-w-5xl mx-auto text-gray-400'>
                 <Swiper
                     spaceBetween={30}
                     centeredSlides={true}
