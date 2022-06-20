65622b58c46ddb582c053fb96ffe84ed8ba6ca96
Igboanugwo Collins <munisco12@gmail.com>
2022-06-20 19:05:41 +0100

    working on customer service

--- pages/customerService.tsx
+++ pages/customerService.tsx
@@ -1,46 +1,83 @@
 import React from 'react'
 
 function customerService() {
-  return (
-      <div className='grid justify-center mt-10 gap-5 pb-10'>
-          <h1
-              className='text-center px-2 py-5 text-3xl font-semibold mb-5'
-              style={{
-                  background: `linear-gradient(145deg, #1c1c30, #171729)`,
-                  boxShadow: `7px 7px 14px #161625,
+    return (
+        <div className='grid justify-center mt-10 gap-5 pb-10'>
+            <h1
+                className='text-center px-2 py-5 text-3xl font-semibold mb-5'
+                style={{
+                    background: `linear-gradient(145deg, #1c1c30, #171729)`,
+                    boxShadow: `7px 7px 14px #161625,
              -7px -7px 14px #1e1e35`,
-                  borderRadius: '50px',
-              }}
-          >
-              CUSTOMER SERVICE
-          </h1>
-          <div className='grid'>
-              <h2 className='text-center font-semibold text-2xl grad p-2'>
-                  Unrivalled Customer Service
-              </h2>
-              <p className='text-justify'>
-                  much of experience within the Mining and Affiliate Industry so
-                  they understand what Miners and Affiliate want and need. So
-                  you can Mine and Affiliate with confidence knowing fully well
-                  that the 1960Token team will always be there to help you
-                  24/hrs a day. 7 days a week.
-              </p>
-              <h2 className='text-center font-semibold text-2xl grad p-2 mt-10'>
-                  Have a question or require specialist assistance?{' '}
-              </h2>
-              <div className='text-justify'>
-                  Our dedicated customer service team is here 24/7 to assist
-                  you. Call Us Email Inquiries +1…. support@1960Token
+                    borderRadius: '50px',
+                }}
+            >
+                CUSTOMER SERVICE
+            </h1>
+            <div className='grid'>
+                <h2 className='text-center font-semibold text-2xl grad p-2'>
+                    Unrivalled Customer Service
+                </h2>
+                <p className='text-justify'>
+                    much of experience within the Mining and Affiliate Industry
+                    so they understand what Miners and Affiliate want and need.
+                    So you can Mine and Affiliate with confidence knowing fully
+                    well that the 1960Token team will always be there to help
+                    you 24/hrs a day. 7 days a week.
+                </p>
+                <h2 className='text-center font-semibold text-2xl grad p-2 mt-10'>
+                    Have a question or require specialist assistance?{' '}
+                </h2>
+                <div className='text-justify'>
+                    Our dedicated customer service team is here 24/7 to assist
+                    you. Call Us Email Inquiries +1…. support@1960Token
+                    <h3 className='grad mt-2'>Send us a message</h3>
+                    Whether you're new to 1960Token platform or have a question
+                    about your existing account, we can help. Simply fill in
+                    your details below and we'll get back to you soon. All
+                    fields are required.
+                    <form>
+                        <div className='mb-4'>
+                            <label
+                                className='block text-gray-700 text-sm font-bold mb-2'
+                                htmlFor='username'
+                            >
+                                Email
+                            </label>
 
-                  <h3 className='grad mt-2'>Send us a message</h3>
-                  Whether you're new to 1960Token platform or have a question
-                  about your existing account, we can help. Simply fill in your
-                  details below and we'll get back to you soon. All fields are
-                  required.
-              </div>
-          </div>
-      </div>
-  )
+                            <input
+                                className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+                                id='email'
+                                type='email'
+                                name='email'
+                                required
+                                //   value={email}
+                                //   onChange={changeHandler}
+                            />
+                        </div>
+                        <div className='mb-4'>
+                            <label
+                                className='block text-gray-700 text-sm font-bold mb-2'
+                                htmlFor='username'
+                            >
+                                Email
+                            </label>
+
+                            <textarea
+                                className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+                                id='textarea'
+                                rows={4}
+                                name='textarea'
+                                required
+                                //   value={email}
+                                //   onChange={changeHandler}
+                            />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default customerService
