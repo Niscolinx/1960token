558fe354ccb91f8835eeed83af4e407dd00d9355
Igboanugwo Collins <munisco12@gmail.com>
2022-06-20 18:26:25 +0100

    working on customer service

--- pages/customerService.tsx
+++ pages/customerService.tsx
@@ -32,7 +32,7 @@ function customerService() {
                   Our dedicated customer service team is here 24/7 to assist
                   you. Call Us Email Inquiries +1…. support@1960Token
 
-                  <h3 className='grad p-2'>Send us a message</h3>
+                  <h3 className='grad mt-2'>Send us a message</h3>
                   Whether you're new to 1960Token platform or have a question
                   about your existing account, we can help. Simply fill in your
                   details below and we'll get back to you soon. All fields are
