e7217ac3e6511f98e6e3720808022da16728d6f0
Igboanugwo Collins <munisco12@gmail.com>
2022-06-20 21:55:37 +0100

    designing home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,8 @@ import Carousel3 from '../public/carousel3.svg'
 import Logo from '../public/logo.svg'
 import Image from 'next/image'
 import { BsPersonPlusFill, BsCheckLg } from 'react-icons/bs'
-import { FaDollarSign } from 'react-icons/fa'
+import { FaDollarSign, FaPeopleArrows } from 'react-icons/fa'
+
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react'
@@ -238,8 +239,20 @@ const Index = () => {
                         </p>
                     </div>
 
-                    <div className="grid">
-                        
+                    <div className='grid'>
+                        <div className='flex'>
+                            <FaPeopleArrows />
+                            <div className='grid'>
+                                <h3>Affiliate Referral</h3>
+                                <p>
+                                    You are handsomely rewarded for telling your
+                                    friends and family about the services of
+                                    1960token. You earn $3 on each successful
+                                    referral. This option enables increase in
+                                    earning
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className='grid'>
