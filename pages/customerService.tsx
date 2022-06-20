39c9208963ef329b9f71afbdaf150d0c3b778182
Igboanugwo Collins <munisco12@gmail.com>
2022-06-20 19:07:16 +0100

    working on customer service

--- pages/customerService.tsx
+++ pages/customerService.tsx
@@ -39,14 +39,14 @@ function customerService() {
                     <form>
                         <div className='mb-4'>
                             <label
-                                className='block text-gray-700 text-sm font-bold mb-2'
+                                className='block text-orange-400 text-sm font-bold mb-2'
                                 htmlFor='username'
                             >
                                 Email
                             </label>
 
                             <input
-                                className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+                                className={`shadow appearance-none border rounded w-full py-4 px-3  mb-3 leading-tight focus:outline-none focus:shadow-outline`}
                                 id='email'
                                 type='email'
                                 name='email'
@@ -57,14 +57,14 @@ function customerService() {
                         </div>
                         <div className='mb-4'>
                             <label
-                                className='block text-gray-700 text-sm font-bold mb-2'
+                                className='block text-orange-400 text-sm font-bold mb-2'
                                 htmlFor='username'
                             >
-                                Email
+                                Message
                             </label>
 
                             <textarea
-                                className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+                                className={`shadow appearance-none border rounded w-full py-4 px-3  mb-3 leading-tight focus:outline-none focus:shadow-outline`}
                                 id='textarea'
                                 rows={4}
                                 name='textarea'
