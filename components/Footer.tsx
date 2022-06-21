e8d79c7cb6e01b9299afa045c818d71026126c06
Igboanugwo Collins <munisco12@gmail.com>
2022-06-21 18:35:38 +0100

    working on toggling dark theme

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,42 +6,55 @@ import { GiTrade } from 'react-icons/gi'
 import { MdOutlineOndemandVideo } from 'react-icons/md'
 
 const Footer = () => {
+    
     return (
-        <div className='bg-[#1a1a2d] text-[#ccccd0] flex justify-between max-w-3xl mx-auto p-2 fixed bottom-0 left-0 right-0 rounded-lg pb-5'>
+        <div className='bg-[#1a1a2d] text-[#ccccd0] flex justify-between max-w-3xl mx-auto p-2 fixed bottom-0 left-0 right-0 rounded-lg pb-5 light:(text-[#1a1a2d] bg-[#ccccd0])'>
             <Link href='/'>
-                <div className='grid justify-items-center p-2 rounded-full cursor-pointer' style={{
+                <div
+                    className='grid justify-items-center p-2 rounded-full cursor-pointer'
+                    style={{
                         background: `linear-gradient(145deg, #1c1c30, #171729)`,
                         boxShadow: ` 5px 5px 16px #0c0c15,
              -5px -5px 16px #282845`,
-                    }}>
-                    <RiHome5Line className='text-orange-300 text-2xl'   />
+                    }}
+                >
+                    <RiHome5Line className='text-orange-300 text-2xl' />
                 </div>
             </Link>
             <Link href='/'>
-                <div className='grid justify-items-center p-2 rounded-full cursor-pointer' style={{
+                <div
+                    className='grid justify-items-center p-2 rounded-full cursor-pointer'
+                    style={{
                         background: `linear-gradient(145deg, #1c1c30, #171729)`,
                         boxShadow: ` 5px 5px 16px #0c0c15,
              -5px -5px 16px #282845`,
-                    }}>
-                    <MdOutlineOndemandVideo className='text-orange-300 text-2xl'   />
+                    }}
+                >
+                    <MdOutlineOndemandVideo className='text-orange-300 text-2xl' />
                 </div>
             </Link>
             <Link href='/'>
-                <div className='grid justify-items-center p-2 rounded-full cursor-pointer' style={{
+                <div
+                    className='grid justify-items-center p-2 rounded-full cursor-pointer'
+                    style={{
                         background: `linear-gradient(145deg, #1c1c30, #171729)`,
                         boxShadow: ` 5px 5px 16px #0c0c15,
              -5px -5px 16px #282845`,
-                    }}>
-                    <GiTrade className='text-orange-300 text-2xl'  />
+                    }}
+                >
+                    <GiTrade className='text-orange-300 text-2xl' />
                 </div>
             </Link>
             <Link href='/dashboard'>
-                <div className='grid justify-items-center p-2 rounded-full cursor-pointer' style={{
+                <div
+                    className='grid justify-items-center p-2 rounded-full cursor-pointer'
+                    style={{
                         background: `linear-gradient(145deg, #1c1c30, #171729)`,
                         boxShadow: ` 5px 5px 16px #0c0c15,
              -5px -5px 16px #282845`,
-                    }}>
-                    <CgProfile className='text-orange-300 text-2xl'  />
+                    }}
+                >
+                    <CgProfile className='text-orange-300 text-2xl' />
                 </div>
             </Link>
         </div>
