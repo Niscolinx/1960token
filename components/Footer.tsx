e4ae528458078990e24189a4507dfd58e42b57b8
Igboanugwo Collins <munisco12@gmail.com>
2022-06-21 18:37:59 +0100

    working on toggling dark theme

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,43 @@
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
-import React from 'react'
+import { useTheme } from 'next-themes'
 import { CgProfile } from 'react-icons/cg'
 import { RiHome5Line } from 'react-icons/ri'
 import { GiTrade } from 'react-icons/gi'
 import { MdOutlineOndemandVideo } from 'react-icons/md'
 
 const Footer = () => {
+    const { theme } = useTheme()
+    const [neuToUse, setNeuToUse] = useState<{}>()
+
+    useEffect(() => {
+        if (theme === 'dark') {
+            console.log('dark theme', { theme })
+
+            setNeuToUse({
+                background: `linear-gradient(145deg, #1c1c30, #171729)`,
+                boxShadow: `5px 5px 16px #0c0c15,
+             -5px -5px 16px #282845`,
+                borderRadius: '50px',
+            })
+        } else {
+            console.log('light theme', { theme })
+            setNeuToUse({
+                background: `linear-gradient(145deg, #dadadf, #b8b8bb)`,
+                boxShadow: `7px 7px 14px #a5a5a8,
+             -7px -7px 14px #f3f3f8`,
+                borderRadius: '50px',
+                color: '#1a1a2d',
+            })
+        }
+    }, [theme])
     
     return (
         <div className='bg-[#1a1a2d] text-[#ccccd0] flex justify-between max-w-3xl mx-auto p-2 fixed bottom-0 left-0 right-0 rounded-lg pb-5 light:(text-[#1a1a2d] bg-[#ccccd0])'>
             <Link href='/'>
                 <div
                     className='grid justify-items-center p-2 rounded-full cursor-pointer'
-                    style={{
-                        background: `linear-gradient(145deg, #1c1c30, #171729)`,
-                        boxShadow: ` 5px 5px 16px #0c0c15,
-             -5px -5px 16px #282845`,
-                    }}
+                    style={neuToUse}
                 >
                     <RiHome5Line className='text-orange-300 text-2xl' />
                 </div>
@@ -24,11 +45,7 @@ const Footer = () => {
             <Link href='/'>
                 <div
                     className='grid justify-items-center p-2 rounded-full cursor-pointer'
-                    style={{
-                        background: `linear-gradient(145deg, #1c1c30, #171729)`,
-                        boxShadow: ` 5px 5px 16px #0c0c15,
-             -5px -5px 16px #282845`,
-                    }}
+                    style={neuToUse}
                 >
                     <MdOutlineOndemandVideo className='text-orange-300 text-2xl' />
                 </div>
@@ -36,11 +53,7 @@ const Footer = () => {
             <Link href='/'>
                 <div
                     className='grid justify-items-center p-2 rounded-full cursor-pointer'
-                    style={{
-                        background: `linear-gradient(145deg, #1c1c30, #171729)`,
-                        boxShadow: ` 5px 5px 16px #0c0c15,
-             -5px -5px 16px #282845`,
-                    }}
+                    style={neuToUse}
                 >
                     <GiTrade className='text-orange-300 text-2xl' />
                 </div>
@@ -48,11 +61,7 @@ const Footer = () => {
             <Link href='/dashboard'>
                 <div
                     className='grid justify-items-center p-2 rounded-full cursor-pointer'
-                    style={{
-                        background: `linear-gradient(145deg, #1c1c30, #171729)`,
-                        boxShadow: ` 5px 5px 16px #0c0c15,
-             -5px -5px 16px #282845`,
-                    }}
+                    style={neuToUse}
                 >
                     <CgProfile className='text-orange-300 text-2xl' />
                 </div>
