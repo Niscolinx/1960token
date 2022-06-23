e5a27c0fcfd6caa6dc291f0ebd552729dfc335be
Igboanugwo Collins <munisco12@gmail.com>
2022-06-23 11:40:20 +0100

    added dayjs to compute date

--- utils/countDownTimer.ts
+++ utils/countDownTimer.ts
@@ -1,33 +1,33 @@
-import dayjs, { Dayjs } from "dayjs"
+import dayjs, { Dayjs } from 'dayjs'
 export const countDownTimerInMs = (milliseconds: number) => {
-
     const dayjsRemainingTimeStamp = dayjs(milliseconds)
     const dayjsNowTimeStamp = dayjs()
 
-    console.log({dayjsRemainingTimeStamp}, {dayjsNowTimeStamp})
+    console.log({ dayjsRemainingTimeStamp }, { dayjsNowTimeStamp })
 
     return {
         hours: hoursRemaining(dayjsNowTimeStamp, dayjsRemainingTimeStamp),
         minutes: minutesRemaining(dayjsNowTimeStamp, dayjsRemainingTimeStamp),
-        seconds: secondsRemaining(dayjsNowTimeStamp, dayjsRemainingTimeStamp)
-        
+        seconds: secondsRemaining(dayjsNowTimeStamp, dayjsRemainingTimeStamp),
     }
 }
 
-
-const hoursRemaining = (now:Dayjs, remainingTime:Dayjs) => {
+const hoursRemaining = (now: Dayjs, remainingTime: Dayjs) => {
     const hours = remainingTime.diff(now, 'hours') % 24
-    return hours
+    return padWithZeros(hours, 2)
 }
-const minutesRemaining = (now:Dayjs, remainingTime:Dayjs) => {
-    const minutes =  remainingTime.diff(now, 'minutes') % 60
+const minutesRemaining = (now: Dayjs, remainingTime: Dayjs) => {
+    const minutes = remainingTime.diff(now, 'minutes') % 60
+    return padWithZeros(minutes, 2)
 }
-const secondsRemaining = (now:Dayjs, remainingTime:Dayjs) => {
-   const seconds = remainingTime.diff(now, 'seconds') % 60
-   return seconds
+const secondsRemaining = (now: Dayjs, remainingTime: Dayjs) => {
+    const seconds = remainingTime.diff(now, 'seconds') % 60
+    return padWithZeros(seconds, 2)
 }
 
-const padWithZeros = (number:number, minLength:number) => {
-const numberString = number.toString()
+const padWithZeros = (number: number, minLength: number) => {
+    const numberString = number.toString()
+    if (numberString.length >= minLength) return numberString
 
+    return '0'.repeat(minLength - numberString.length) + numberString
 }
