0721039beb1e0499d1caeac77aa887b775f1ca2c
Igboanugwo Collins <munisco12@gmail.com>
2022-06-23 09:10:58 +0100

    building the countdown timer from scratch

--- utils/countDownTimer.ts
+++ utils/countDownTimer.ts
@@ -1,5 +1,7 @@
 import dayjs from "dayjs"
 export const countDownTimerInMs = (milliseconds: number) => {
+    const dayjsRemainingTimeStamp = dayjs(milliseconds)
+    const dayjsNowTimeStamp = dayjs()
 
 
     return 994234
