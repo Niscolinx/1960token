62b2bf629544aa8a7c47eb3c15f8ffa116b395f0
Igboanugwo Collins <munisco12@gmail.com>
2022-06-24 21:03:18 +0100

    working on video

--- pages/dashboard/videoAds.tsx
+++ pages/dashboard/videoAds.tsx
@@ -4,10 +4,10 @@ import ReactPlayer from 'react-player/youtube'
 function videoAds() {
     return (
         <div className='grid justify-center h-[78vh]'>
-            <div className='grid'>
+            <div className='grid self-center'>
                 <ReactPlayer
                     url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
-                    width='300px'
+                    width='100%'
                 />
             </div>
         </div>
