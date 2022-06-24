c86252cc97b5e3865bc7894662bca4a18f568b1a
Igboanugwo Collins <munisco12@gmail.com>
2022-06-24 21:00:43 +0100

    working on video

--- pages/dashboard/videoAds.tsx
+++ pages/dashboard/videoAds.tsx
@@ -7,10 +7,7 @@ function videoAds() {
             <div className='grid'>
                 <ReactPlayer
                     url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
-                    style={{
-                        width: '100%',
-                        height: '100%',
-                    }}
+                   
                 />
             </div>
         </div>
