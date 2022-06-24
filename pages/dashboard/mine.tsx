d8e4863f4293616719434c18d1f7d8c4a9231d73
Igboanugwo Collins <munisco12@gmail.com>
2022-06-24 18:35:53 +0100

    working on mine

--- pages/dashboard/mine.tsx
+++ pages/dashboard/mine.tsx
@@ -5,78 +5,80 @@ import React, { useEffect, useState } from 'react'
 import CountDownTimer from '../../components/countDownTimer'
 
 function mine() {
-        const { data: session } = useSession()
+    const { data: session } = useSession()
 
-     const [miningStart, setMiningStart] = useState(false)
-     const [prevTimeStore, setPrevTimeStore] = useState<Dayjs>()
+    const [miningStart, setMiningStart] = useState(false)
+    const [prevTimeStore, setPrevTimeStore] = useState<Dayjs>()
 
-     useEffect(() => {
-         const getTimeStore = localStorage.getItem('miningTime')
-         console.log('initial load')
+    useEffect(() => {
+        const getTimeStore = localStorage.getItem('miningTime')
+        console.log('initial load')
 
-         if (!getTimeStore) {
-             console.log('load the gettime')
-             axios
-                 .post('/api/startMining', { session })
-                 .then(({ data }) => {
-                     console.log({ data })
-                     const { miningStart, isMining } = data
-                     if (isMining) {
-                         localStorage.setItem('miningTime', miningStart)
-                         setMiningStart(true)
-                         setPrevTimeStore(dayjs(miningStart))
-                     }
-                 })
-                 .catch((err) => {
-                     console.log({ err })
-                 })
-         } else {
-             console.log("Didn't load the get time")
+        if (!getTimeStore) {
+            console.log('load the gettime')
+            axios
+                .post('/api/startMining', { session })
+                .then(({ data }) => {
+                    console.log({ data })
+                    const { miningStart, isMining } = data
+                    if (isMining) {
+                        localStorage.setItem('miningTime', miningStart)
+                        setMiningStart(true)
+                        setPrevTimeStore(dayjs(miningStart))
+                    }
+                })
+                .catch((err) => {
+                    console.log({ err })
+                })
+        } else {
+            console.log("Didn't load the get time")
 
-             const dayjsNowTimeStamp = dayjs()
-             if (dayjsNowTimeStamp.isAfter(dayjs(getTimeStore))) {
-                 console.log('Timer finished')
-                 return setMiningStart(false)
-             }
-             setPrevTimeStore(dayjs(getTimeStore))
-             setMiningStart(true)
-         }
-     }, [])
+            const dayjsNowTimeStamp = dayjs()
+            if (dayjsNowTimeStamp.isAfter(dayjs(getTimeStore))) {
+                console.log('Timer finished')
+                return setMiningStart(false)
+            }
+            setPrevTimeStore(dayjs(getTimeStore))
+            setMiningStart(true)
+        }
+    }, [])
 
-     const handleStart = () => {
-         const dayjsRemainingTimeStamp = dayjs().add(12, 'hours')
+    const handleStart = () => {
+        const dayjsRemainingTimeStamp = dayjs().add(12, 'hours')
 
-         const getTimeStore = localStorage.getItem('miningTime')
+        const getTimeStore = localStorage.getItem('miningTime')
 
-         if (!getTimeStore) {
-             const remainingTime = dayjsRemainingTimeStamp
+        if (!getTimeStore) {
+            const remainingTime = dayjsRemainingTimeStamp
 
-             axios
-                 .post('/api/startMining', { session, remainingTime })
-                 .then(({ data }) => {
-                     const { miningStart } = data
-                     localStorage.setItem('miningTime', miningStart)
-                     setPrevTimeStore(dayjs(miningStart))
-                 })
-                 .catch((err) => {
-                     console.log({ err })
-                 })
-         } else {
-             console.log('handle start already')
-         }
-     }
-  return (
-      <div className='grid justify-center items-center h-1/3 bg-red-50' >
-          {prevTimeStore && (
-              <CountDownTimer
-                  hour={12}
-                  start={miningStart}
-                  prevTimeStore={prevTimeStore}
-              />
-          )}
-          <button onClick={handleStart}>Start</button>
-      </div>
-  )
+            axios
+                .post('/api/startMining', { session, remainingTime })
+                .then(({ data }) => {
+                    const { miningStart } = data
+                    localStorage.setItem('miningTime', miningStart)
+                    setPrevTimeStore(dayjs(miningStart))
+                })
+                .catch((err) => {
+                    console.log({ err })
+                })
+        } else {
+            console.log('handle start already')
+        }
+    }
+    return (
+        <div className='grid justify-center h-[50vh] bg-red-50 self-center '>
+            <div className='grid self-center'>
+                {prevTimeStore && (
+                    <CountDownTimer
+                        hour={12}
+                        start={miningStart}
+                        prevTimeStore={prevTimeStore}
+                    />
+                )}
+                <button onClick={handleStart}>Start</button>
+            </div>
+        </div>
+    )
 }
 
 export default mine
