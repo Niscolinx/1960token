49ef587a7383107fab52918c2678b2e29f728d4d
Igboanugwo Collins <munisco12@gmail.com>
2022-06-24 18:46:44 +0100

    working on mine

--- pages/dashboard/mine.tsx
+++ pages/dashboard/mine.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import dayjs, { Dayjs } from 'dayjs'
 import { useSession } from 'next-auth/react'
+import { useTheme } from 'next-themes'
 import React, { useEffect, useState } from 'react'
 import CountDownTimer from '../../components/countDownTimer'
 
@@ -9,6 +10,42 @@ function mine() {
 
     const [miningStart, setMiningStart] = useState(false)
     const [prevTimeStore, setPrevTimeStore] = useState<Dayjs>()
+
+    const { theme } = useTheme()
+    const [neuToUse, setNeuToUse] = useState<{}>()
+    const [stepsNeu, setStepsNeu] = useState<{}>()
+
+    useEffect(() => {
+        if (theme === 'dark') {
+            console.log('dark theme', { theme })
+
+            setNeuToUse({
+                background: `linear-gradient(145deg, #1c1c30, #171729)`,
+                boxShadow: `7px 7px 14px #161625,
+                 -7px -7px 14px #1e1e35`,
+                borderRadius: '50px',
+            })
+
+            setStepsNeu({})
+        } else {
+            console.log('light theme', { theme })
+            setNeuToUse({
+                background: `linear-gradient(145deg, #dadadf, #b8b8bb)`,
+                boxShadow: `7px 7px 14px #a5a5a8,
+             -7px -7px 14px #f3f3f8`,
+                borderRadius: '50px',
+                color: '#1a1a2d',
+            })
+
+            setStepsNeu({
+                background: `linear-gradient(145deg, #dadadf, #b8b8bb)`,
+                boxShadow: `7px 7px 14px #a5a5a8,
+             -7px -7px 14px #f3f3f8`,
+                borderRadius: '50px',
+                color: '#1a1a2d',
+            })
+        }
+    }, [theme])
 
     useEffect(() => {
         const getTimeStore = localStorage.getItem('miningTime')
