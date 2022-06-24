a08828a8c9268a8a17dc86352b7d288721ec824a
Igboanugwo Collins <munisco12@gmail.com>
2022-06-24 20:08:57 +0100

    working on mine

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,30 @@
+import { useTheme } from 'next-themes'
 import { Html, Head, Main, NextScript } from 'next/document'
+import { useState, useEffect } from 'react'
 
 export default function Document() {
+    const { theme } = useTheme()
+    const [htmlStyle, setHtmlStyle] = useState<{}>()
+
+    useEffect(() => {
+        if (theme === 'dark') {
+            console.log('dark theme', { theme })
+
+            setHtmlStyle({
+                background: `#1a1a2d`,
+                borderRadius: '50px',
+            })
+        } else {
+            console.log('light theme', { theme })
+            setHtmlStyle({
+                background: `linear-gradient(145deg, #dadadf, #b8b8bb)`,
+                boxShadow: `7px 7px 14px #a5a5a8,
+             -7px -7px 14px #f3f3f8`,
+                borderRadius: '50px',
+                color: '#1a1a2d',
+            })
+        }
+    }, [theme])
     return (
         <Html style={{background: 'red !important'}}>
             <Head />
