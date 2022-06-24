562072bb45f599dc79fe0e526c0b1ab37c0007a5
Igboanugwo Collins <munisco12@gmail.com>
2022-06-24 19:30:54 +0100

    working on mine

--- pages/dashboard/mine.tsx
+++ pages/dashboard/mine.tsx
@@ -102,7 +102,7 @@ function mine() {
         }
     }
     return (
-        <div className='grid justify-center h-[82vh] self-center relative'>
+        <div className='grid justify-center h-[82vh] self-center'>
             <div
                 className='w-full h-full bg-gray-700 opacity-[20] absolute top-0 bottom-0 left-0 right-0'
                 style={{ display: isDim ? 'block' : 'none' }}
