51371e6ad675d9d5168424c81d13e096d2deb0b6
Igboanugwo Collins <munisco12@gmail.com>
2022-06-24 20:56:39 +0100

    working on video

--- pages/dashboard/videoAds.tsx
+++ pages/dashboard/videoAds.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import ReactPlayer from 'react-player/youtube' 
-
+import ReactPlayer from 'react-player/youtube'
 
 function videoAds() {
-  return (
-      <div className='grid justify-center'>
-          
-
-          <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' style={{
-            width: '100%',
-            height: '100%'
-          }}/>
-      </div>
-  )
+    return (
+        <div className='grid justify-center h-[78vh]'>
+            <div className='grid'>
+                <ReactPlayer
+                    url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                    }}
+                />
+            </div>
+        </div>
+    )
 }
 
 export default videoAds
