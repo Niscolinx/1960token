a12ce00c96cda484ab2f503799eae84425ea7009
Igboanugwo Collins <munisco12@gmail.com>
2022-06-24 20:17:31 +0100

    working on mine

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,13 +2,12 @@ import { useTheme } from 'next-themes'
 import { Html, Head, Main, NextScript } from 'next/document'
 import { useState, useEffect } from 'react'
 
-const { theme } = useTheme()
-const [htmlStyle, setHtmlStyle] = useState<{}>()
+
 export default function Document() {
 
    
     return (
-        <Html style={htmlStyle}>
+        <Html>
             <Head />
             <title>1960token</title>
             <body>
