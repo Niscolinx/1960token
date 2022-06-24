5c3c72842f00183e87bae8b550f2c451610e9112
Igboanugwo Collins <munisco12@gmail.com>
2022-06-24 20:14:19 +0100

    working on mine

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,24 +2,12 @@ import { useTheme } from 'next-themes'
 import { Html, Head, Main, NextScript } from 'next/document'
 import { useState, useEffect } from 'react'
 
+
 export default function Document() {
-    const { theme } = useTheme()
-    const [htmlStyle, setHtmlStyle] = useState<{}>()
 
-    useEffect(() => {
-        if (theme === 'dark') {
-
-            setHtmlStyle({
-                background: '#1a1a2d !important',
-            })
-        } else {
-           setHtmlStyle({
-               background: '#ccccd0 !important',
-           })
-        }
-    }, [theme])
+    
     return (
-        <Html style={htmlStyle}>
+        <Html>
             <Head />
             <title>1960token</title>
             <body>
