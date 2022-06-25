532e5007710fdb6176ebc96badfb302f6b1f6b40
Igboanugwo Collins <munisco12@gmail.com>
2022-06-25 17:41:30 +0100

    combined video and mine into one page

--- pages/dashboard/earn.tsx
+++ pages/dashboard/earn.tsx
@@ -117,7 +117,7 @@ function earn() {
                     <p className='font-bold text-3xl'>0.0001</p>
                 </div>
 
-                <div className='grid w-full relative z-0'>
+                <div className='grid w-full'>
                     <ReactPlayer
                         url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
                         width='100%'
