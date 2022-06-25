a44f87cb9d405232464e237ea487d22d8812286a
Igboanugwo Collins <munisco12@gmail.com>
2022-06-25 16:52:54 +0100

    combined video and mine into one page

--- components/nav.tsx
+++ components/nav.tsx
@@ -62,16 +62,12 @@ function nav({ session }: { session: Session | null }) {
   
     return (
         <nav className='flex justify-between items-center px-4'>
-            {router.asPath.includes('earn') ? (
-                <p>Mining</p>
-            ) : (
-                <>
-                    {isSession}
-                    <button className='px-2 py-2 border rounded-lg self-center flex'>
-                        {renderTheme()}
-                    </button>
-                </>
-            )}
+            <>
+                {isSession}
+                <button className='px-2 py-2 border rounded-lg self-center flex'>
+                    {renderTheme()}
+                </button>
+            </>
         </nav>
     )
 }
