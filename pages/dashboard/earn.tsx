275a040e8e5007066fbee7eae946bd7e1811234a
Igboanugwo Collins <munisco12@gmail.com>
2022-06-25 16:26:26 +0100

    combined video and mine into one page

--- pages/dashboard/earn.tsx
+++ pages/dashboard/earn.tsx
@@ -110,7 +110,7 @@ function earn() {
                 <ReactPlayer
                     url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
                     width='100%'
-                    height='50rem'
+                    height='40rem'
                    
                 />
             </div>
