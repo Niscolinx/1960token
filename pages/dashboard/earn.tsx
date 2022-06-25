465a78ed9b2481042ba2f5102cd0895aa3c22ba2
Igboanugwo Collins <munisco12@gmail.com>
2022-06-25 20:18:14 +0100

    added slider images

--- pages/dashboard/earn.tsx
+++ pages/dashboard/earn.tsx
@@ -124,8 +124,14 @@ function earn() {
                         height='40rem'
                     />
                 </div>
-                <div className='absolute grid right-0 left-0 mr-4 justify-items-center top-1/5 gap-6 z-90 justify-end cursor-pointer bg-red-300'>
-                 
+                <div className='absolute grid right-0 left-0 mr-4 justify-items-center top-1/5 gap-6 z-90 justify-end cursor-pointer '>
+                    <div className='grid text-white justify-center justify-items-center'>
+                        <IoIosPeople
+                            className='text-white text-4xl'
+                            onClick={() => console.log('team clicked')}
+                        />
+                        <p className='font-bold'>1/1</p>
+                    </div>
                     <div className='grid text-white justify-center justify-items-center'>
                         <GiTrade
                             onClick={handleStart}
@@ -133,8 +139,8 @@ function earn() {
                             // className='text-white text-4xl animate-spin animate-duration-[4s]'
                         />
                         <HiOutlineArrowNarrowUp
-                            className='text-white text-3xl animate-bounce mt-1 flex'
-                            // style={{ display: isDim ? 'block' : 'none' }}
+                            className='text-white text-3xl animate-bounce mt-1'
+                            style={{ display: isDim ? 'block' : 'none' }}
                         />
                     </div>
                 </div>
