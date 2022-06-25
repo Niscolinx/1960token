631a833b1eed9206e6423d1001f5db072ab91eab
Igboanugwo Collins <munisco12@gmail.com>
2022-06-25 18:12:53 +0100

    combined video and mine into one page

--- pages/dashboard/earn.tsx
+++ pages/dashboard/earn.tsx
@@ -83,7 +83,7 @@ function earn() {
     }, [])
 
     const handleStart = () => {
-        console.log('clicked on mine')
+        console.log("clicked on mine")
         setIsDim(false)
         const dayjsRemainingTimeStamp = dayjs().add(12, 'hours')
 
@@ -123,28 +123,24 @@ function earn() {
                         width='100%'
                         height='40rem'
                     />
-                    <div className='absolute grid right-0 left-0 bottom-0 top-0 mr-4 justify-items-center top-1/5 gap-6 z-10 justify-end'>
-                        <div className='grid'>
-                            <div className=''>
-                                <GiTrade
-                                    onClick={handleStart}
-                                    className='text-white text-4xl'
-                                    // className='text-white text-4xl animate-spin animate-duration-[4s]'
-                                />
-                                <HiOutlineArrowNarrowUp
-                                    className='text-white text-3xl animate-bounce mt-1'
-                                    style={{
-                                        display: isDim ? 'block' : 'none',
-                                    }}
-                                />
-                            </div>
-                            <div className='grid text-white justify-center justify-items-center'>
-                                <IoIosPeople
-                                    className='text-white text-4xl'
-                                    onClick={() => console.log('team clicked')}
-                                />
-                                <p className='font-bold'>1/1</p>
-                            </div>
+                    <div className='absolute grid right-0 left-0 mr-4 justify-items-center top-1/5 gap-6 z-10 justify-end'>
+                        <div className=''>
+                            <GiTrade
+                                onClick={handleStart}
+                                className='text-white text-4xl'
+                                // className='text-white text-4xl animate-spin animate-duration-[4s]'
+                            />
+                            <HiOutlineArrowNarrowUp
+                                className='text-white text-3xl animate-bounce mt-1'
+                                style={{ display: isDim ? 'block' : 'none' }}
+                            />
+                        </div>
+                        <div className='grid text-white justify-center justify-items-center'>
+                            <IoIosPeople
+                                className='text-white text-4xl'
+                                onClick={() => console.log('team clicked')}
+                            />
+                            <p className='font-bold'>1/1</p>
                         </div>
                     </div>
                 </div>
