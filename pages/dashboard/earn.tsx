1ff94907ba938769b4be3b9636adb9c7b182e2fd
Igboanugwo Collins <munisco12@gmail.com>
2022-06-25 17:55:01 +0100

    combined video and mine into one page

--- pages/dashboard/earn.tsx
+++ pages/dashboard/earn.tsx
@@ -125,10 +125,21 @@ function earn() {
                     />
                     <div className='absolute grid right-0 left-0 mr-4 justify-items-center top-1/5 gap-6 z-10 justify-end'>
                         <div className='grid text-white justify-center justify-items-center'>
-                            <IoIosPeople className='text-white text-4xl' onClick={handleStart} />
+                            <IoIosPeople className='text-white text-4xl' onClick={() => console.log("team clicked")} />
                             <p className='font-bold'>1/1</p>
                         </div>
 
+                        <div className=''>
+                            <GiTrade
+                                onClick={handleStart}
+                                className='text-white text-4xl'
+                                // className='text-white text-4xl animate-spin animate-duration-[4s]'
+                            />
+                            <HiOutlineArrowNarrowUp
+                                className='text-white text-3xl animate-bounce mt-1'
+                                style={{ display: isDim ? 'block' : 'none' }}
+                            />
+                        </div>
                     </div>
                 </div>
 
