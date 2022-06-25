11b25bcc5f191dd6dee41a0c342410ba53719995
Igboanugwo Collins <munisco12@gmail.com>
2022-06-25 16:08:10 +0100

    combined video and mine into one page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ const Layout: React.FC<{}> = ({ children }) => {
     const { data: session } = useSession()
 
     return (
-        <div className='py-2 px-4 bg-[#1a1a2d] text-[#ccccd0] mx-auto relative light:(bg-[#ccccd0] text-[#1a1a2d])'>
+        <div className='py-2 bg-[#1a1a2d] text-[#ccccd0] mx-auto relative light:(bg-[#ccccd0] text-[#1a1a2d])'>
             <Nav session={session} />
             <main className='mb-25'>{children}</main>
             <Footer />
