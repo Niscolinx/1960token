5d9f29c7b2ee7a86202f15513dcd7d052dfc9d6f
Igboanugwo Collins <munisco12@gmail.com>
2022-06-27 19:38:54 +0100

    working on user registration

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs'
 import { NextApiRequest, NextApiResponse } from 'next'
 import jwt from 'jsonwebtoken'
 import User, { IUser } from '../../../models/User'
+import dbConnect from '../../../lib/dbConnect'
 
 export default async function login(req: NextApiRequest, res: NextApiResponse) {
     console.log('login...', req.body)
@@ -9,16 +10,21 @@ export default async function login(req: NextApiRequest, res: NextApiResponse) {
     const { emailOrUsername, password } = req.body
 
     try {
-        const email: IUser | null = await User.findOne({ email: emailOrUsername })
-        const username: IUser | null = await User.findOne({ username: emailOrUsername })
+        await dbConnect()
+
+        const email: IUser | null = await User.findOne({
+            email: emailOrUsername,
+        })
+        const username: IUser | null = await User.findOne({
+            username: emailOrUsername,
+        })
 
         if (!email || !username) {
             return res.status(400).json('invalid user')
-        }else{
-            console.log({email})
-            console.log({username})
+        } else {
+            console.log({ email })
+            console.log({ username })
         }
-        
 
         const checkPassword = await bcrypt.compare(password, user!.password)
         if (!checkPassword) {
@@ -30,14 +36,15 @@ export default async function login(req: NextApiRequest, res: NextApiResponse) {
                 email,
                 userId: user!._id.toString(),
             },
-            process.env.JWT_SECRET!, {
-                expiresIn: '1hr'
+            process.env.JWT_SECRET!,
+            {
+                expiresIn: '1hr',
             }
         )
 
         return res.status(200).json({
             user,
-            token
+            token,
         })
     } catch (err) {
         console.log({ err })
