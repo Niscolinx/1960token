80de9a74529dc104b906d08d809ac1bcee52a066
Igboanugwo Collins <munisco12@gmail.com>
2022-06-27 15:18:57 +0100

    working on redux action

---
+++ src/app/actions/mine.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import dayjs from 'dayjs';
+import { Session } from 'next-auth';
+export async function startMine(session: Session): Promise<{ data: number }> {
+    const dayjsRemainingTimeStamp = dayjs().add(12, 'hours')
+
+    const getTimeStore = localStorage.getItem('miningTime')
+
+    if (!getTimeStore) {
+        const remainingTime = dayjsRemainingTimeStamp
+
+        axios
+            .post('/api/startMining', { session, remainingTime })
+            .then(({ data }) => {
+                const { miningStart } = data
+                localStorage.setItem('miningTime', miningStart)
+                //setPrevTimeStore(dayjs(miningStart))
+            })
+            .catch((err) => {
+                console.log({ err })
+            })
+    } else {
+        console.log('handle start already')
+    }
+return {
+    data:1
+}
+}
