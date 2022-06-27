dfd1b7df6a394c26d055ec32e4df3f92f9144257
Igboanugwo Collins <munisco12@gmail.com>
2022-06-27 18:52:55 +0100

    working on user registration

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -1,66 +1,68 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next'
 import bcrypt from 'bcryptjs'
-import dbConnect from '../../../lib/dbConnect';
-import User from '../../../models/User';
+import dbConnect from '../../../lib/dbConnect'
+import User from '../../../models/User'
 
-
-async function signupHandler(req:NextApiRequest, res:NextApiResponse) {
-
-    try{
+async function signupHandler(req: NextApiRequest, res: NextApiResponse) {
+    try {
         await dbConnect()
 
-    if (req.method === 'POST') {
-        console.log("checking")
-        //Getting email and password from body
-        const { email, password, username, phoneNumber } = req.body
-        //Validate
-        if (!email || !email.includes('@') || !password || !phoneNumber) {
-            console.log('failed')
-            res.status(422).json({ message: 'Invalid Data' })
-            return
-        }
+        if (req.method === 'POST') {
+            console.log('checking')
+            //Getting email and password from body
+            const { email, password, username, phoneNumber } = req.body
+            //Validate
+            if (!email || !email.includes('@') || !password || !phoneNumber) {
+                console.log('failed')
+                res.status(422).json({ message: 'Invalid Data' })
+                return
+            }
 
-        const existingUser = await User.findOne({ email })
-        const existingUsername = await User.findOne({
-            username
-        })
-        const existingPhoneNumber = await User.findOne({phoneNumber})
-
-       if (existingUser) {
-           const error = new Error('User already exists')
-           throw error
-       }
-
-       if (existingUsername) {
-           throw new Error('Username already taken')
-       }
-
-       if(existingPhoneNumber){
-        throw new Error('Number already in use')
-       }
-
-        const storeUser = new User({
-            email,
-            password: await bcrypt.hash(password, 12),
-            username,
-            phoneNumber
-        })
-
-        const verifyStored = await storeUser.save()
-        console.log({verifyStored})
-
-        if(verifyStored){
-            res.status(201).json({
-                message: 'successful'
+            const existingUser = await User.findOne({ email })
+            const existingUsername = await User.findOne({
+                username,
             })
-        }
+            const existingPhoneNumber = await User.findOne({ phoneNumber })
 
-    } else {
-        //Response for other than POST method
-        res.status(500).json({ message: 'Route not valid' })
-    }}
-    catch(err) {
-        console.log({err})
+            if (existingUser) {
+                const error = new Error('User already exists')
+                throw error
+            }
+
+            if (existingUsername) {
+                throw new Error('Username already taken')
+            }
+
+            if (existingPhoneNumber) {
+                throw new Error('Number already in use')
+            }
+
+            const storeUser = new User({
+                email,
+                password: await bcrypt.hash(password, 12),
+                username,
+                phoneNumber,
+            })
+
+            const verifyStored = await storeUser.save()
+            console.log({ verifyStored })
+
+            if (verifyStored) {
+                res.status(201).json({
+                    message: 'successful',
+                })
+            }
+            else{
+                res.status(404).json({
+                    message: 'failed'
+                })
+            }
+        } else {
+            //Response for other methods other than POST
+            res.status(500).json({ message: 'Route not valid' })
+        }
+    } catch (err) {
+        console.log({ err })
     }
 }
 
