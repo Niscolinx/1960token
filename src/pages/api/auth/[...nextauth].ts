6c3ad5494a487a3878d109265f203d6accd80fd2
Igboanugwo Collins <munisco12@gmail.com>
2022-06-27 21:18:04 +0100

    working on user login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -57,7 +57,6 @@ export default NextAuth({
     callbacks: {
         jwt: async ({ token, user }) => {
             user && (token.user = user)
-            console.log('*******************',{token})
             return token
         },
         session: async ({ session, token }) => {
