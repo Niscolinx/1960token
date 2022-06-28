9da94a989209725f6ab9e42728589f4819756eec
Igboanugwo Collins <munisco12@gmail.com>
2022-06-28 18:52:15 +0100

    working on mining

--- src/features/mine/MinerSlice.ts
+++ src/features/mine/MinerSlice.ts
@@ -2,7 +2,7 @@ import { nextAuthSession } from './../../lib/types';
 import dayjs, { Dayjs } from 'dayjs';
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 
-import type { AppState, AppThunk } from '../../app/store'
+import type { AppState, AppThunk, MineState } from '../../app/store'
 import { startMining } from './MineAPI'
 
 export interface CounterState {
@@ -66,17 +66,17 @@ export const { increment, decrement, initMine } = MiningSlice.actions
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectMining = (state: AppState) => state.mine.startedMining
+export const selectMining = (state: MineState) => state.mine.startedMining
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
-export const incrementIfOdd =
-    (amount: number): AppThunk =>
-    (dispatch, getState) => {
-        const currentValue = selectCount(getState())
-        if (currentValue % 2 === 1) {
-            dispatch(incrementByAmount(amount))
-        }
-    }
+// export const incrementIfOdd =
+//     (amount: number): AppThunk =>
+//     (dispatch, getState) => {
+//         const currentValue = selectCount(getState())
+//         if (currentValue % 2 === 1) {
+//             dispatch(incrementByAmount(amount))
+//         }
+//     }
 
-export default counterSlice.reducer
+export default MiningSlice.reducer
