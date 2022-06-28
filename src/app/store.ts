cc55fe340a31223540fd56d5f977506845cd7d42
Igboanugwo Collins <munisco12@gmail.com>
2022-06-28 18:48:58 +0100

    working on mining

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,25 +1,26 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
 
 import counterReducer from '../features/counter/counterSlice'
-
+import MiningReducer from '../features/mine/MinerSlice'
 
 export function makeStore() {
-  return configureStore({
-    reducer: { counter: counterReducer },
-  })
+    return configureStore({
+        reducer: { counter: counterReducer, mine: MiningReducer },
+    })
 }
 
 const store = makeStore()
 
 export type AppState = ReturnType<typeof store.getState>
+export type MineState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch
 
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  AppState,
-  unknown,
-  Action<string>
+    ReturnType,
+    AppState,
+    unknown,
+    Action<string>
 >
 
 export default store
