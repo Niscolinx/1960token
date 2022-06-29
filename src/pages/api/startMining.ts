0705b8fb572a6eec0561f9390aa4854efd9594c3
Igboanugwo Collins <munisco12@gmail.com>
2022-06-29 08:44:42 +0100

    working on mining

--- src/pages/api/startMining.ts
+++ src/pages/api/startMining.ts
@@ -2,43 +2,41 @@ import dayjs from 'dayjs'
 import { NextApiRequest, NextApiResponse } from 'next'
 import User, { IUser } from '../../models/User'
 
- interface returnTypeJson {
-     miningStartedAt: string
-     isMining: boolean
- }
+interface returnTypeJson {
+    miningStartedAt: string
+    isMining: boolean
+}
 
- type JsonType<returnTypeJson> = (body: returnTypeJson) => object
+type JsonType<returnTypeJson> = (body: returnTypeJson) => object
 
- export interface TypedReqBody<T> extends NextApiResponse {
+export interface TypedReqBody<T> extends NextApiResponse {
     json: JsonType<T>
- }
+}
 
 export default async function StartMining(
     req: NextApiRequest,
     res: TypedReqBody<returnTypeJson>
 ) {
-  
     const startTimeStamp = dayjs().add(12, 'hours')
 
     const { email }: IUser = req.body.user
 
     const user = await User.findOne({ email })
-    
 
     console.log({ user })
 
     if (user.isMining) {
         console.log('mining started already')
-       return res.json({
-           miningStartedAt: user.miningStartedAt,
-           isMining: true,
-       })
+        return res.json({
+            miningStartedAt: user.miningStartedAt,
+            isMining: true,
+        })
     } else {
-        console.log('start user mining', {startTimeStamp})
-        user.miningStartedAt = JSON.stringify(startTimeStamp)
+        console.log('start user mining', { startTimeStamp })
+        user.miningStartedAt = startTimeStamp
         const updatedUser = await user.save()
 
-        console.log({updatedUser})
+        console.log({ updatedUser })
 
         return res.json({
             isMining: user.isMining,
