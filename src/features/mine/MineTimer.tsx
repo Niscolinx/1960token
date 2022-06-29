12659e3fe79d46b1dceb6961f1aa17d3244edad9
Igboanugwo Collins <munisco12@gmail.com>
2022-06-29 15:38:37 +0100

    working on mining

--- src/features/mine/MineTimer.tsx
+++ src/features/mine/MineTimer.tsx
@@ -5,8 +5,6 @@ import { useTheme } from 'next-themes'
 import { increment } from '../../features/mine/MinerSlice'
 import { useAppDispatch } from '../../app/hooks'
 
-
-
 const defaultTimer = {
     hours: '00',
     minutes: '00',
@@ -19,46 +17,48 @@ interface ICountDown {
 }
 
 const CountDownTimer = ({ start, prevTimeStore }: ICountDown) => {
-        const dispatch = useAppDispatch()
-
+    const dispatch = useAppDispatch()
 
     const [remainingTime, setRemainingTime] = useState(defaultTimer)
 
-    const updateRemainingTimer = ( prev?: Dayjs) => {
-        console.log("mining update")
-        setRemainingTime(countDown(prev))
+    useEffect(() => {
         dispatch(increment())
+    }, [remainingTime])
+
+    const updateRemainingTimer = (prev?: Dayjs) => {
+        console.log('mining update')
+        setRemainingTime(countDown(prev))
     }
-const { theme } = useTheme()
-const [neuToUse, setNeuToUse] = useState<{}>()
+    const { theme } = useTheme()
+    const [neuToUse, setNeuToUse] = useState<{}>()
 
-useEffect(() => {
-    if (theme === 'dark') {
-        console.log('dark theme', { theme })
+    useEffect(() => {
+        if (theme === 'dark') {
+            console.log('dark theme', { theme })
 
-        setNeuToUse({
-            background: `linear-gradient(145deg, #1c1c30, #171729)`,
-            boxShadow: `7px 7px 14px #161625,
+            setNeuToUse({
+                background: `linear-gradient(145deg, #1c1c30, #171729)`,
+                boxShadow: `7px 7px 14px #161625,
                  -7px -7px 14px #1e1e35`,
-            borderRadius: '50px',
-        })
-    } else {
-        console.log('light theme', { theme })
-        setNeuToUse({
-            background: `linear-gradient(145deg, #dadadf, #b8b8bb)`,
-            boxShadow: `7px 7px 14px #a5a5a8,
+                borderRadius: '50px',
+            })
+        } else {
+            console.log('light theme', { theme })
+            setNeuToUse({
+                background: `linear-gradient(145deg, #dadadf, #b8b8bb)`,
+                boxShadow: `7px 7px 14px #a5a5a8,
              -7px -7px 14px #f3f3f8`,
-            borderRadius: '50px',
-            color: '#1a1a2d',
-        })
-    }
-}, [theme])
+                borderRadius: '50px',
+                color: '#1a1a2d',
+            })
+        }
+    }, [theme])
 
     useEffect(() => {
         if (start) {
-            console.log("started")
+            console.log('started')
             const intervalId = setInterval(() => {
-                            dispatch(increment())
+                dispatch(increment())
 
                 return updateRemainingTimer(prevTimeStore)
             }, 1000)
