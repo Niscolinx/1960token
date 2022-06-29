b8221a9c5d54abb53146456bb31eba859a024e9e
Igboanugwo Collins <munisco12@gmail.com>
2022-06-29 21:42:03 +0100

    added stop mining

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,9 +1,6 @@
-import type { ChangeEvent } from 'react'
-import { useEffect, useRef } from 'react'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
-import type { AppDispatch } from './store'
-
+import type { AppDispatch, AppState } from './store'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>()
