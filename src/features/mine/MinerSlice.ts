830573eaf47a428db3642209faa43615aaead409
Igboanugwo Collins <munisco12@gmail.com>
2022-06-29 09:04:11 +0100

    working on mining

--- src/features/mine/MinerSlice.ts
+++ src/features/mine/MinerSlice.ts
@@ -39,16 +39,16 @@ export const MiningSlice = createSlice({
     initialState,
     // The `reducers` field lets us define reducers and generate associated actions
     reducers: {
-        increment: (state) => {
+       // increment: (state) => {
             // Redux Toolkit allows us to write "mutating" logic in reducers. It
             // doesn't actually mutate the state because it uses the Immer library,
             // which detects changes to a "draft state" and produces a brand new
             // immutable state based off those changes
-            state.value += 1
-        },
-        decrement: (state) => {
-            state.value -= 1
-        },
+        //     state.value += 1
+        // },
+        // decrement: (state) => {
+        //     state.value -= 1
+        // },
         // Use the PayloadAction type to declare the contents of `action.payload`
         // initMineTime: (state, action: PayloadAction<string>) => {
         //     state.startedMining = action.payload
@@ -67,12 +67,12 @@ export const MiningSlice = createSlice({
     },
 })
 
-export const { increment, decrement } = MiningSlice.actions
+//export const { increment, decrement } = MiningSlice.actions
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectMining = (state: MineState) => state.mine.startedMining
+export const selectMining = (state: MineState) => state.mine.miningStartedAt
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
