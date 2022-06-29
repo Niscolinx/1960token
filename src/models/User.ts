62f91a0e9bb60210e396eeacfdb1ca17dce9aa15
Igboanugwo Collins <munisco12@gmail.com>
2022-06-29 08:36:22 +0100

    working on mining

--- src/models/User.ts
+++ src/models/User.ts
@@ -86,6 +86,6 @@ const userSchema = new Schema<IUser>(
     { timestamps: true }
 )
 
-console.log(models.user)
+console.log('models user', models.user)
 
 export default models.User || model('User', userSchema)
