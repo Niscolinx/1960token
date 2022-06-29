7b32d675b1bb0c177730e42ef9d9363854a8785a
Igboanugwo Collins <munisco12@gmail.com>
2022-06-29 10:20:08 +0100

    working on mining

--- src/features/mine/MinerSlice.ts
+++ src/features/mine/MinerSlice.ts
@@ -78,7 +78,7 @@ export const MiningSlice = createSlice({
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectMining = (state: MineState) => state.mine.miningStartedAt
+export const selectMining = (state: MineState) => state.mine
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
