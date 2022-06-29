cf02602b1ef050b97c1d56def03b45e1a153a444
Igboanugwo Collins <munisco12@gmail.com>
2022-06-29 07:30:55 +0100

    working on mining

--- src/features/mine/MinerSlice.ts
+++ src/features/mine/MinerSlice.ts
@@ -8,13 +8,13 @@ import { startMining } from './MineAPI'
 export interface MiningState {
     value: number
     status: 'idle' | 'loading' | 'failed'
-    startedMining?: string
+    startedMining: Dayjs
 }
 
 const initialState: MiningState = {
     value: 0,
     status: 'idle',
- startedMining: JSON.stringify(dayjs().add(12, 'hours')),
+ startedMining: dayjs().add(12, 'hours'),
 }
 
 // The function below is called a thunk and allows us to perform async logic. It
