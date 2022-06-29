17474510594c25796f6bef611d11150c98243fbd
Igboanugwo Collins <munisco12@gmail.com>
2022-06-29 20:45:05 +0100

    added stop mining

---
+++ src/pages/api/stopMining.ts
@@ -0,0 +1,41 @@
+import dayjs from 'dayjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+import User, { IUser } from '../../models/User'
+
+interface returnTypeJson {
+    miningStartedAt: string
+    isMining: boolean
+}
+
+type JsonType<returnTypeJson> = (body: returnTypeJson) => object
+
+export interface TypedReqBody<T> extends NextApiResponse {
+    json: JsonType<T>
+}
+
+export default async function StartMining(
+    req: NextApiRequest,
+    res: TypedReqBody<returnTypeJson>
+) {
+    const startTimeStamp = dayjs().add(12, 'hours')
+
+    const { email }: IUser = req.body.user
+
+    const user = await User.findOne({ email })
+
+    if (user.isMining) {
+        return res.json({
+            miningStartedAt: user.miningStartedAt,
+            isMining: user.isMining,
+        })
+    } else {
+        user.miningStartedAt = startTimeStamp
+        user.isMining = true
+        await user.save()
+
+        return res.json({
+            isMining: user.isMining,
+            miningStartedAt: user.miningStartedAt,
+        })
+    }
+}
