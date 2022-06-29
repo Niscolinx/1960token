394cf00033494c060d083094033084b3f2c1aebd
Igboanugwo Collins <munisco12@gmail.com>
2022-06-29 09:47:10 +0100

    working on mining

--- src/pages/api/startMining.ts
+++ src/pages/api/startMining.ts
@@ -17,7 +17,6 @@ export default async function StartMining(
     req: NextApiRequest,
     res: TypedReqBody<returnTypeJson>
 ) {
-    console.log(req.body)
     const startTimeStamp = dayjs().add(12, 'hours')
 
     const { email }: IUser = req.body.user
