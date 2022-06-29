cf94c49568051b45ec7748c4262da0ab0cd303f8
Igboanugwo Collins <munisco12@gmail.com>
2022-06-29 09:20:57 +0100

    working on mining

--- src/features/mine/MineAPI.ts
+++ src/features/mine/MineAPI.ts
@@ -1,10 +1,14 @@
 import axios, { AxiosResponse } from 'axios'
 import { nextAuthSession } from '../../lib/types'
 
+interface IFetchedData {
+    isMining: boolean
+    miningStatedAt: string
+}
 
 export async function startMining(
     session: nextAuthSession
-): Promise<AxiosResponse<void>> {
+): Promise<IFetchedData> {
     const response = await axios.post('/api/startMining', session)
 
     return response.data 
