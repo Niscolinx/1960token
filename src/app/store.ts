74d63b3cba4d120bc0aad76aa725b90dc768d4f8
Igboanugwo Collins <munisco12@gmail.com>
2022-06-30 14:52:08 +0100

    added stop mining

--- src/app/store.ts
+++ src/app/store.ts
@@ -14,7 +14,6 @@ const persistedReducer = persistReducer(persistConfig, MiningReducer)
 export const store = configureStore({
     reducer: { mine: persistedReducer },
     devTools: process.env.NODE_ENV !== 'production',
-    middleware: [thunk]
 })
 
 export const persistedStore = persistStore(store)
