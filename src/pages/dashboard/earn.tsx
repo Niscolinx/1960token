5613bb74b5ad888f8a5ab29692952becd4b747fd
Igboanugwo Collins <munisco12@gmail.com>
2022-06-30 16:05:37 +0100

    added stop mining

--- src/pages/dashboard/earn.tsx
+++ src/pages/dashboard/earn.tsx
@@ -75,9 +75,9 @@ function earn() {
         if (session) {
             console.log('the session')
             if(persistedStorage){
-                console.log('persist storage', JSON.parse(persistedStorage))
                 const parsedJSON = JSON.parse(persistedStorage)
-                
+
+                console.log('parsed json', parsedJSON)
             }
             // if (!getTimeStore) {
             //     console.log('run code')
