16d1ee86dbb2b42e066cb44bced7a90d93cb6e96
Igboanugwo Collins <munisco12@gmail.com>
2022-06-30 17:00:40 +0100

    working on countdown miner display

--- src/features/mine/MineCountDown.ts
+++ src/features/mine/MineCountDown.ts
@@ -1,6 +1,26 @@
 import dayjs, { Dayjs } from 'dayjs'
 
 
+const hoursRemaining = (now: Dayjs, remainingTime: Dayjs) => {
+    const hours = remainingTime.diff(now, 'hours') % 24
+    return padWithZeros(hours, 2)
+}
+const minutesRemaining = (now: Dayjs, remainingTime: Dayjs) => {
+    const minutes = remainingTime.diff(now, 'minutes') % 60
+    return padWithZeros(minutes, 2)
+}
+const secondsRemaining = (now: Dayjs, remainingTime: Dayjs) => {
+    const seconds = remainingTime.diff(now, 'seconds') % 60
+    return padWithZeros(seconds, 2)
+}
+
+const padWithZeros = (number: number, minLength: number) => {
+    const numberString = number.toString()
+    if (numberString.length >= minLength) return numberString
+
+    return '0'.repeat(minLength - numberString.length) + numberString
+}
+
 export const MineCountDown = (prevTimeStore?: Dayjs) => {
     
     const dayjsNowTimeStamp = dayjs()
@@ -38,25 +58,7 @@ export const totalMinedTokens = (prevTimeStore?: Dayjs) => {
             minutes: '00',
             seconds: '00',
         }
-    }
+    }}
 
 
-const hoursRemaining = (now: Dayjs, remainingTime: Dayjs) => {
-    const hours = remainingTime.diff(now, 'hours') % 24
-    return padWithZeros(hours, 2)
-}
-const minutesRemaining = (now: Dayjs, remainingTime: Dayjs) => {
-    const minutes = remainingTime.diff(now, 'minutes') % 60
-    return padWithZeros(minutes, 2)
-}
-const secondsRemaining = (now: Dayjs, remainingTime: Dayjs) => {
-    const seconds = remainingTime.diff(now, 'seconds') % 60
-    return padWithZeros(seconds, 2)
-}
 
-const padWithZeros = (number: number, minLength: number) => {
-    const numberString = number.toString()
-    if (numberString.length >= minLength) return numberString
-
-    return '0'.repeat(minLength - numberString.length) + numberString
-}
