b3bbac62f3e5abdfc643664dd08d3837ffacdcb2
Igboanugwo Collins <munisco12@gmail.com>
2022-07-01 16:13:17 +0100

    working landing page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Footer from './Footer'
 import Nav from './nav'
 import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 const Layout: React.FC<{}> = ({ children }) => {
     const { data: session } = useSession()
+    const router = useRouter()
 
+    useEffect(() => {
+        if(router.asPath.includes("earn")){
+
+        }
+    }, [router])
     return (
         <div className='py-2 bg-[#1a1a2d] text-[#ccccd0] mx-auto relative light:(bg-[#ccccd0] text-[#1a1a2d])'>
             <Nav session={session} />
-            <main>{children}</main>
+            <main className='mb-25'>{children}</main>
             <Footer />
         </div>
     )
