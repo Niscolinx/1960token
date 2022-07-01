e4c61a8434259e24982753eeb3fe3de0b8fa9b8c
Igboanugwo Collins <munisco12@gmail.com>
2022-07-01 08:40:53 +0100

    working on countdown token

--- src/features/mine/MineCountDown.ts
+++ src/features/mine/MineCountDown.ts
@@ -23,7 +23,7 @@ const padWithZeros = (number: number, minLength: number) => {
 
 //token utils
 const IncrementSeconds = (now: Dayjs, remainingTime: Dayjs) => {
-    const seconds = remainingTime.diff(now, 'second')
+    const seconds = remainingTime.diff(now, 'seconds')
     return padWithDecimalZeros(seconds, 4)
 }
 
