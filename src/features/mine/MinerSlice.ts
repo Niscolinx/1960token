4e11f6b87330ecd758d5f4109a99922c9c90eb88
Igboanugwo Collins <munisco12@gmail.com>
2022-07-01 14:58:55 +0100

    working on countdown token

--- src/features/mine/MinerSlice.ts
+++ src/features/mine/MinerSlice.ts
@@ -108,7 +108,7 @@ export const MiningSlice = createSlice({
     },
 })
 
-export const { increment, increaseTokenCount} = MiningSlice.actions
+export const { increment, increaseTokenCount, flush} = MiningSlice.actions
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
