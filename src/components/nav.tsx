18c228b3795509fa848ae33fff399c2278247d1c
Igboanugwo Collins <munisco12@gmail.com>
2022-07-02 19:46:48 +0100

    working on landing page

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -57,7 +57,7 @@ function nav({ session }: { session: nextAuthSession | null }) {
             {router.asPath.includes('earn') ? null : (
                 <>
                     {isSession}
-                    <div className='flex gap-4'>
+                    <div className='flex gap-4 bg-red-400'>
                         <Link
                             href={`/api/auth/${isAuth ? 'signout' : 'signin'}`}
                         >
