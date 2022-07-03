42ef44d1ea16b6602771cae6bc8a4605d9c42670
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 00:02:43 +0100

    working on video

--- src/store/features/user/UserSlice.ts
+++ src/store/features/user/UserSlice.ts
@@ -1,13 +1,12 @@
-import { AppState } from '../../app/store';
+import { AppState } from '../../app/store'
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 import { nextAuthSession } from '../../../lib/types'
-import { IUser } from '../../../models/User';
-import { userData } from './UserApi';
-
+import { IUser } from '../../../models/User'
+import { userData } from './UserApi'
 
 const initialState: IUser = {
-     _id: {},
+    _id: {},
     username: '',
     email: '',
     phoneNumber: '',
@@ -16,7 +15,7 @@ const initialState: IUser = {
     role: '',
     totalMined: 0,
     portfolio: 0,
-    status: 'idle' ,
+    status: 'idle',
     password: '',
     pendingWithdrawals: [],
     pendingDeposits: [],
@@ -45,17 +44,17 @@ export const UserSlice = createSlice({
             })
             .addCase(getUser.fulfilled, (state, action) => {
                 console.log('user payload', action.payload)
-                const payload:{} = action.payload
+                const payload: {} = action.payload
                 state.status = 'success'
 
-                for(let i in payload){
-                  state[i] = payload[i]
-                }
-            //     type userFields = keyof IUser
+                // for(let i in payload){
+                //   state[i] = payload[i]
+                // }
+                type userFields = keyof IUser
 
-            //    Object.entries(payload).forEach((field: [userFields, any]) => {
-            //     state[field[0]] = field[1]
-            //    })
+                Object.entries(payload).forEach((field) => {
+                    state[field[0]] = field[1]
+                })
             })
             .addCase(getUser.rejected, (state) => {
                 state.status = 'failed'
