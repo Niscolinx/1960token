81b499093a7c2ac828e8c5efeb55ee0a7a0c5a0a
Igboanugwo Collins <munisco12@gmail.com>
2022-07-03 21:31:13 +0100

    working on video

--- src/store/features/video/VideoSlice.ts
+++ src/store/features/video/VideoSlice.ts
@@ -14,19 +14,12 @@ const initialState: videoState = {
     status: 'idle',
     totalMined: 0.0,
 }
-
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
 export const initVideoEnded = createAsyncThunk(
     'video',
     async (userSession: nextAuthSession) => {
         const response = await videoEnded(userSession)
 
         console.log({ response })
-        // The value we return becomes the `fulfilled` action payload
         return response
     }
 )
