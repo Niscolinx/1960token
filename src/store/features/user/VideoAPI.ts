b794cc3c1d749f4fc89207ddbd4d3f329e141a02
Igboanugwo Collins <munisco12@gmail.com>
2022-07-03 22:47:16 +0100

    working on video

--- src/store/features/user/VideoAPI.ts
+++ src/store/features/user/VideoAPI.ts
@@ -1,14 +1,11 @@
 import axios from 'axios'
 import { nextAuthSession } from '../../../lib/types'
-
-interface IFetchedData {
-    totalMined: number
-}
+import { IUser } from '../../../models/User'
 
 export async function videoEnded(
     session: nextAuthSession
-): Promise<IFetchedData> {
-    const response = await axios.post('/api/videoEnded', session)
+): Promise<IUser> {
+    const response = await axios.post('/api/user', session)
 
     return response.data
 }
