d943834531433f797be887dedeee52afd42cd8c0
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 00:22:47 +0100

    working on video

--- src/store/features/user/UserSlice.ts
+++ src/store/features/user/UserSlice.ts
@@ -51,12 +51,10 @@ export const UserSlice = createSlice({
                 //   state[i] = payload[i]
                 // }
 
-                // Object.entries(payload).forEach((field) => {
-                //     const key = field[0] as 'username'
-                //     state[key] = field[1] as string 
-                // })
-
-                state.totalMined = payload.totalMined
+                Object.entries(payload).forEach((field) => {
+                    const key = field[0] as 'username'
+                    state[key] = field[1] as string 
+                })
             })
             .addCase(getUser.rejected, (state) => {
                 state.status = 'failed'
