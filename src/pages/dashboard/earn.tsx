a66679817fc552d111ef3865d2b3be1657c7ae5d
Igboanugwo Collins <munisco12@gmail.com>
2022-07-03 22:22:12 +0100

    working on video

--- src/pages/dashboard/earn.tsx
+++ src/pages/dashboard/earn.tsx
@@ -73,10 +73,11 @@ function earn() {
 
         if (session) {
             const parsedJSON = JSON.parse(persistedStorage!)
+            console.log('parsedJson', parsedJSON.mine)
 
-            if (JSON.parse(parsedJSON.mine.miningSession) === 'stall') {
-                dispatch(initMineAsync(session))
-            }
+            // if (JSON.parse(parsedJSON.mine.miningSession) === 'stall') {
+            //     dispatch(initMineAsync(session))
+            // }
 
             if (dayjsNowTimeStamp.isAfter(dayjs(mineState.miningStartedAt))) {
                 console.log('stop mining')
