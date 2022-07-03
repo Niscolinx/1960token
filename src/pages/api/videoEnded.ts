576c08b7aa53d2666215a8019bbfb3f21bb5ebaf
Igboanugwo Collins <munisco12@gmail.com>
2022-07-03 21:45:32 +0100

    working on video

---
+++ src/pages/api/videoEnded.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import User, { IUser } from '../../models/User'
+
+interface returnTypeJson {
+    totalMined: number
+}
+
+type JsonType<returnTypeJson> = (body: returnTypeJson) => object
+
+export interface TypedReqBody<T> extends NextApiResponse {
+    json: JsonType<T>
+}
+
+export default async function StartMining(
+    req: NextApiRequest,
+    res: TypedReqBody<returnTypeJson>
+) {
+    const { email }: IUser = req.body.user
+
+    const user = await User.findOne({ email })
+
+    console.log('the user', user)
+
+    user.miningStartedAt = ''
+    user.isMining = false
+    user.totalMined = user.totalMined + 0.5
+    await user.save()
+
+    return res.json({
+        isMining: user.isMining,
+        miningStartedAt: user.miningStartedAt,
+        totalMined: user.totalMined,
+    })
+}
