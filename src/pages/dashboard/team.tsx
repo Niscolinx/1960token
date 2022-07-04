141d8a770293f8776bf039ffea2fef91a00bcc80
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 22:05:47 +0100

    working on team

--- src/pages/dashboard/team.tsx
+++ src/pages/dashboard/team.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {BsFillFilePersonFill} from 'react-icons/bs'
 
 function team() {
   return (
     <div>
         <h1>My Team</h1>
-        
+
     </div>
   )
 }
