79fc403ec419716e3d2dc14040d35ffdd8c42367
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 08:17:00 +0100

    working on animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -172,7 +172,7 @@ const Index = () => {
                 <div className='grid justify-center gap-5 my-10 md:grid-cols-[repeat(auto-fit,minmax(20rem,100px))]'>
                     <AnimationOnScroll
                         animateOnce={false}
-                        animateIn='animateLeft'
+                        animateIn='fadeInLeft'
                     >
                         <div
                             className='grid items-center bg-[#ccccd0] text-black py-2 px-4 rounded-lg w-full grid-cols-[min-content,1fr] '
