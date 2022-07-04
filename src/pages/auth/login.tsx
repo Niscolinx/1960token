162b37a9df69b82b8a262a3a365b79fb1793217e
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 10:48:48 +0100

    working on animations

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -145,7 +145,7 @@ const Login = ({ providers }: LoginProps) => {
                         </label>
 
                         <input
-                            className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-red-500 ${
+                            className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-white ${
                                 error && errorFields.includes('email')
                                     ? 'border-red-500'
                                     : ''
@@ -166,7 +166,7 @@ const Login = ({ providers }: LoginProps) => {
                             Password
                         </label>
                         <input
-                            className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline ${
+                            className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-white ${
                                 error && errorFields.includes('password')
                                     ? 'border-red-500'
                                     : ''
