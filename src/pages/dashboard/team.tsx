b1cca28d681212ea3995088b27d55895aba8e23c
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 22:18:46 +0100

    working on team

--- src/pages/dashboard/team.tsx
+++ src/pages/dashboard/team.tsx
@@ -29,7 +29,7 @@ function team() {
     <div className='py-5 px-2 grid justify-center justify-items-center'>
         <h1 style={neuToUse} className=' justify-self-center text-2xl px-2 py-1 mt-4 mb-5'>My Team</h1>
         <div className="grid justify-items-center">
-            <BsFillFilePersonFill className='text-4xl'/>
+            <BsFillFilePersonFill className='text-4xl mb-2'/>
 
             <div className="grid">
                 <h3 className='text-xl'>Upline: <span className='font-semibold'>Tester1</span></h3>
