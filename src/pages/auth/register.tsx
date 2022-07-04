f336db3a8f7228c4c0060898935812c87ba4d82e
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 22:28:42 +0100

    working on team

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -255,11 +255,7 @@ const Register = () => {
                         Referral
                     </label>
                     <input
-                        className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-white ${
-                            error && errorFields.includes('confirmPassword')
-                                ? 'border-red-500'
-                                : ''
-                        }`}
+                        className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-white`}
                         id='confirmPassword'
                         name='confirmPassword'
                         type='password'
