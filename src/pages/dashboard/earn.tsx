07ad6583fc6c96f8bcb3cdab98e248b4c52c4771
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 13:56:31 +0100

    working on animations

--- src/pages/dashboard/earn.tsx
+++ src/pages/dashboard/earn.tsx
@@ -171,7 +171,7 @@ function earn() {
                                 }`}
                             />
                             <HiOutlineArrowNarrowUp
-                                className='text-white dark:text-[#1a1a2d] text-3xl animate-bounce mt-4 animate-duration-[2s]'
+                                className='text-white dark:text-[#1a1a2d] text-3xl animate-bounce mt-7 animate-duration-[2s]'
                                 style={{ display: isDim ? 'block' : 'none' }}
                             />
                         </div>
