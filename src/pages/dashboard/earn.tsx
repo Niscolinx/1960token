f916a173b6bf766f93b1eb8d16ae504b60613e43
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 13:57:40 +0100

    working on animations

--- src/pages/dashboard/earn.tsx
+++ src/pages/dashboard/earn.tsx
@@ -170,12 +170,13 @@ function earn() {
                                         : ''
                                 }`}
                             />
-                            <div className="grid">
+                            <div className="grid justify-center just">
 
                             <HiOutlineArrowNarrowUp
                                 className='text-white dark:text-[#1a1a2d] text-3xl animate-bounce mt-7 animate-duration-[4s]'
                                 style={{ display: isDim ? 'block' : 'none' }}
                                 />
+                                <span>Click to mine!</span>
                                 </div>
                         </div>
                     </div>
