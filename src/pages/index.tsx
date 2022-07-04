ab8eb6434849e0b0f1819f52e3aeed5c8151da95
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 13:22:02 +0100

    working on animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -91,7 +91,7 @@ const Index = () => {
                                     <Link href='/api/auth/signin'>
                                         <button className='bg-orange-300 text-[#1a1a2d] rounded px-2 py-1 justify-self-center text-sm animateBottom'>
                                             Get Started
-                                        </button>{' '}
+                                        </button>
                                     </Link>
                                 </div>
                             </div>
@@ -117,7 +117,7 @@ const Index = () => {
                                 <Link href='/api/auth/signin'>
                                     <button className='bg-orange-300 text-[#1a1a2d] rounded px-2 py-1 justify-self-center text-md animateBottom'>
                                         Get Started
-                                    </button>{' '}
+                                    </button>
                                 </Link>
                             </div>
                         </div>
@@ -137,7 +137,7 @@ const Index = () => {
                                 <Link href='/api/auth/signin'>
                                     <button className='bg-orange-300 text-[#1a1a2d] rounded px-2 py-1 justify-self-center text-sm animateBottom'>
                                         Get Started
-                                    </button>{' '}
+                                    </button>
                                 </Link>
                             </div>
                         </div>
