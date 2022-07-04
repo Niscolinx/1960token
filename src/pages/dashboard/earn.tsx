4c5d3f4a5d94e5333e0c7ed1daf03c8531f21236
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 13:48:52 +0100

    working on animations

--- src/pages/dashboard/earn.tsx
+++ src/pages/dashboard/earn.tsx
@@ -148,9 +148,9 @@ function earn() {
                 </div>
                 <AnimationOnScroll
                     initiallyVisible={true}
-                    animateIn='animate__animate__rotateInDownLeft'
+                    animateIn='animate__rotateInDownLeft'
                 >
-                    <div className='absolute grid right-0 mr-4 justify-items-center top-1/5 gap-6 z-90 justify-end cursor-pointer bg-[#1a1a2d] rounded animateLeft'>
+                    <div className='absolute grid right-0 mr-4 justify-items-center top-1/5 gap-6 z-90 justify-end cursor-pointer bg-[#1a1a2d] dark:bg-gray-300 rounded animateLeft'>
                         <div className='grid text-white justify-center justify-items-center'>
                             <IoIosPeople
                                 className='text-white text-4xl'
