e537247f47e5574b2d746a94db9750af8f2cd696
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 15:19:07 +0100

    working on earning page

--- src/pages/dashboard/earn.tsx
+++ src/pages/dashboard/earn.tsx
@@ -14,9 +14,7 @@ import {
     initStopMineAsync,
 } from '../../store/features/mine/MinerSlice'
 import MineTimer from '../../store/features/mine/MineTimer'
-import {
-    initVideoEnded,
-} from '../../store/features/video/VideoSlice'
+import { initVideoEnded } from '../../store/features/video/VideoSlice'
 import { getUser, selectUser } from '../../store/features/user/UserSlice'
 import { AnimationOnScroll } from 'react-animation-on-scroll'
 
@@ -47,7 +45,7 @@ function earn() {
         }
     }, [session])
 
-    console.log({fetchedUser})
+    console.log({ fetchedUser })
 
     useEffect(() => {
         if (theme === 'dark') {
@@ -90,8 +88,7 @@ function earn() {
         if (session) {
             const parsedJSON = JSON.parse(persistedStorage!)
 
-            if(persistedStorage){
-
+            if (persistedStorage) {
                 if (JSON.parse(parsedJSON.mine).miningSession === 'stall') {
                     dispatch(initMineAsync(session))
                     setIsDim(false)
@@ -138,58 +135,60 @@ function earn() {
                 <div className='grid self-center'>
                     <MineTimer start={miningStart} />
                 </div>
-
-                <AnimationOnScroll
-                    initiallyVisible={true}
-                    animateIn='animate__zoomInUp'
-                >
-                    <div className='absolute grid right-0 mr-4 justify-items-center top-1/5 gap-6 z-90 justify-end cursor-pointer bg-[#1a1a2d] dark:bg-gray-300 rounded animateLeft p-2'>
-                        <div className='grid text-white justify-center justify-items-center'>
-                            <IoIosPeople
-                                className='text-white text-4xl dark:text-[#1a1a2d]'
-                                onClick={() => console.log('team clicked')}
-                            />
-                            <p className='font-bold dark:text-[#1a1a2d]'>1/1</p>
-                        </div>
-                        <div className='grid text-white justify-center justify-items-center'>
-                            <GiTrade
-                                onClick={handleMineStart}
-                                // className='text-white text-4xl'
-                                className={`text-white dark:text-[#1a1a2d] text-4xl ${
-                                    !isDim && miningStart
-                                        ? 'animate-spin animate-duration-[4s]'
-                                        : ''
-                                }`}
-                            />
-                            <div className='grid justify-center justify-items-center'>
-                                <HiOutlineArrowNarrowUp
-                                    className='text-white dark:text-[#1a1a2d] text-3xl animate-bounce mt-7 animate-duration-[4s]'
-                                    style={{
-                                        display: isDim ? 'block' : 'none',
-                                    }}
+                <div className='grid'>
+                    <AnimationOnScroll
+                        initiallyVisible={true}
+                        animateIn='animate__zoomInUp'
+                    >
+                        <div className='absolute grid right-0 mr-4 justify-items-center top-1/5 gap-6 z-90 justify-end cursor-pointer bg-[#1a1a2d] dark:bg-gray-300 rounded animateLeft p-2'>
+                            <div className='grid text-white justify-center justify-items-center'>
+                                <IoIosPeople
+                                    className='text-white text-4xl dark:text-[#1a1a2d]'
+                                    onClick={() => console.log('team clicked')}
                                 />
-                                <span
-                                    className='dark:text-[#1a1a2d] text-white'
-                                    style={{
-                                        display: isDim ? 'block' : 'none',
-                                    }}
-                                >
-                                    Click to mine!
-                                </span>
+                                <p className='font-bold dark:text-[#1a1a2d]'>
+                                    1/1
+                                </p>
+                            </div>
+                            <div className='grid text-white justify-center justify-items-center'>
+                                <GiTrade
+                                    onClick={handleMineStart}
+                                    // className='text-white text-4xl'
+                                    className={`text-white dark:text-[#1a1a2d] text-4xl ${
+                                        !isDim && miningStart
+                                            ? 'animate-spin animate-duration-[4s]'
+                                            : ''
+                                    }`}
+                                />
+                                <div className='grid justify-center justify-items-center'>
+                                    <HiOutlineArrowNarrowUp
+                                        className='text-white dark:text-[#1a1a2d] text-3xl animate-bounce mt-7 animate-duration-[4s]'
+                                        style={{
+                                            display: isDim ? 'block' : 'none',
+                                        }}
+                                    />
+                                    <span
+                                        className='dark:text-[#1a1a2d] text-white'
+                                        style={{
+                                            display: isDim ? 'block' : 'none',
+                                        }}
+                                    >
+                                        Click to mine!
+                                    </span>
+                                </div>
                             </div>
                         </div>
+                    </AnimationOnScroll>
+                    <div className='relative z-10'>
+                        <ReactPlayer
+                            url='https://youtu.be/HoPNOczYU4Y'
+                            width='100%'
+                            height='100%'
+                            //onEnded={handleVideoEnded}
+                            onStart={handleVideoEnded}
+                        />
                     </div>
-                </AnimationOnScroll>
-                <div className='relative z-10'>
-                    <ReactPlayer
-                        url='https://youtu.be/HoPNOczYU4Y'
-                        width='100%'
-                        height='100%'
-                        //onEnded={handleVideoEnded}
-                        onStart={handleVideoEnded}
-                    />
                 </div>
-
                 <div
                     className='w-full h-full bg-black bg-opacity-80 absolute top-0 bottom-0 left-0 right-0 z-20'
                     style={{ display: isDim ? 'block' : 'none' }}
