19d91bd4424d5c7e378db05ec3d7cbacb0d08f0a
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 08:44:03 +0100

    working on animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -269,7 +269,10 @@ const Index = () => {
                             </div>
                         </div>
                         <div className='grid grid-cols-[max-content,1fr] md:(flex flex-col items-center grid-cols-none) gap-5'>
+                                                        <AnimationOnScroll animateIn='animate__slideInLeft'>
+
                             <FiAirplay className='text-blue-400 light:text-[#1a1a2d] text-6xl' />
+                            </AnimationOnScroll>
                             <div className='grid md:justify-items-center'>
                                 <h3 className='text-2xl font-semibold text-white pb-2 light:text-black'>
                                     Entertainment
