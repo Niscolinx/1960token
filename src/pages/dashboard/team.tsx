5f554a99f843a8638d42de1ea4638e0de938a33c
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 22:23:58 +0100

    working on team

--- src/pages/dashboard/team.tsx
+++ src/pages/dashboard/team.tsx
@@ -42,7 +42,7 @@ function team() {
                     </h3>
                 </div>
             </div>
-            <div className='grid mt-5 border border-gray-500 rounded-lg'>
+            <div className='grid mt-5 border border-gray-500 rounded-lg py-2 px-5'>
                 <div className='grid'>
                     <p>
                         Total Referral Commission
