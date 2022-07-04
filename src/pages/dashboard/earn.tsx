257ee91739fe80ea1d8b2738bf031e31f869f42c
Igboanugwo Collins <munisco12@gmail.com>
2022-07-04 15:57:13 +0100

    working on earning page

--- src/pages/dashboard/earn.tsx
+++ src/pages/dashboard/earn.tsx
@@ -136,10 +136,10 @@ function earn() {
                     <MineTimer start={miningStart} />
                 </div>
                 <div className='relative'>
-                    <div className='absolute grid right-0 mr-4 justify-items-center top-1/5 gap-6 z-90 justify-end cursor-pointer bg-[#1a1a2d] dark:bg-gray-300 rounded p-2'>
+                    <div className='absolute grid right-0 mr-4 justify-items-center top-1/5 gap-6 z-90 justify-end cursor-pointer bg-[#1a1a2d] dark:bg-gray-300 rounded p-2 animateLeft'>
                         <AnimationOnScroll
                             initiallyVisible={true}
-                            animateIn='animate__slideInTop'
+                            animateIn='animate__slideInLeft'
                         >
                             <div className='grid text-white justify-center justify-items-center'>
                                 <IoIosPeople
