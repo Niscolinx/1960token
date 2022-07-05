c4e0bb90da5696b51f52a50e27849702342f4547
Igboanugwo Collins <munisco12@gmail.com>
2022-07-05 20:52:42 +0100

    increase height

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -33,7 +33,7 @@ const Home = () => {
         }
     }, [theme])
     return (
-        <div className='h-[71vh] px-4'>
+        <div className='h-[93vh] px-4'>
             <div className='grid mt-10 gap-5'>
                 <div
                     className=' justify-center grid w-max text-center py-2 px-10 rounded-lg place-self-center'
