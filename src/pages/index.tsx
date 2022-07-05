5d6a48d53ce042dad51b19c811ee2a91d071b149
Igboanugwo Collins <munisco12@gmail.com>
2022-07-05 20:15:59 +0100

    added footer image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -327,7 +327,6 @@ const Index = () => {
                     src='footer_crypto.jpeg'
                     alt=''
                     style={{
-                        position: 'absolute',
                         height: '7rem',
                         width: '100%',
                         objectFit: 'cover',
@@ -335,7 +334,7 @@ const Index = () => {
                     }}
                 />
                 <div className='absolute z-40 left-0 right-0 top-0 bottom-0'>
-                    <div className='grid text-white justify-center justify-items-center items-center'>
+                    <div className='grid text-white justify-center justify-items-center items-center place-items-center'>
                         <h1>GET STARTED TODAY WITH</h1>
                         <p>1960TOKEN</p>
                         <p>Open an account today and start trading!</p>
