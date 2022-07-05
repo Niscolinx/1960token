e554d989bd2ed300f0f00e87d42a4969ef3ed629
Igboanugwo Collins <munisco12@gmail.com>
2022-07-05 20:16:52 +0100

    added footer image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -333,7 +333,7 @@ const Index = () => {
                         zIndex: 10,
                     }}
                 />
-                <div className='absolute z-40 left-0 right-0 top-0 bottom-0'>
+                <div className='absolute z-40 left-0 right-0 top-0 bottom-0 grid'>
                     <div className='grid text-white justify-center justify-items-center items-center place-items-center'>
                         <h1>GET STARTED TODAY WITH</h1>
                         <p>1960TOKEN</p>
