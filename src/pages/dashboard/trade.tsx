ee29b3e419096e09583b04e126a55f00a306c30f
Igboanugwo Collins <munisco12@gmail.com>
2022-07-06 18:32:32 +0100

    added tradingview widget

--- src/pages/dashboard/trade.tsx
+++ src/pages/dashboard/trade.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AdvancedRealChart from '../../widgets/AdvancedRealChart'
+import TechnicalAnalysis from '../../widgets/LiveTicker'
 
 function trade() {
   return (
     <div className='h-[90vh]'>trade
     <AdvancedRealChart/>
+    <TechnicalAnalysis/>
     </div>
   )
 }
