35b50aed51ca1b849addda4b02839c3b206b0905
Igboanugwo Collins <munisco12@gmail.com>
2022-07-07 07:27:37 +0100

    added tradingview widget

--- utils/custom.d.ts
+++ utils/custom.d.ts
@@ -3,7 +3,6 @@ import { Connection } from 'mongoose'
 declare global {
     var mongoose: any
 }
-declare module 'react-tradingview-widget'
 
 export const mongoose = global.mongoose || new Connection()
 
