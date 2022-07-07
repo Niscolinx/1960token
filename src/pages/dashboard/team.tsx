c3ccf5dbb73bca224de02c85d8de76edd08df082
Igboanugwo Collins <munisco12@gmail.com>
2022-07-07 08:24:41 +0100

    removed tradingview widget

--- src/pages/dashboard/team.tsx
+++ src/pages/dashboard/team.tsx
@@ -29,7 +29,8 @@ function team() {
             <div className='py-5 px-2 grid justify-center justify-items-center'>
                 <div className="flex">
                     <div className="grid">
-                        <p>Total Incomes</p>
+                        <p>Referral Income</p>
+                        <p>0</p>
                     </div>
                 </div>
             </div>
