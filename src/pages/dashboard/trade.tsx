b67c6b0ae7f3f6cd5c5354331d9d5a1be6e54ea7
Igboanugwo Collins <munisco12@gmail.com>
2022-07-07 10:03:17 +0100

    working on team referral

--- src/pages/dashboard/trade.tsx
+++ src/pages/dashboard/trade.tsx
@@ -5,6 +5,30 @@ function trade() {
     return (
         <div className='h-[90vh]'>
             trade
+
+<div className="tradingview-widget-container">
+  <div id="tradingview_3800e"></div>
+  <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/BTCUSDT/?exchange=BINANCE" rel="noopener" target="_blank"><span className="blue-text">BTCUSDT Chart</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+  new TradingView.widget(
+  {
+  "autosize": true,
+  "symbol": "BINANCE:BTCUSDT",
+  "interval": "1",
+  "timezone": "Etc/UTC",
+  "theme": "dark",
+  "style": "2",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "hide_top_toolbar": true,
+  "save_image": false,
+  "container_id": "tradingview_3800e"
+&rbrace;
+  );
+  </script>
+</div>
         </div>
     )
 }
