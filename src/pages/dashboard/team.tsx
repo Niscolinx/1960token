560bb0ac1711088c3d56247abbb19121dfade869
Igboanugwo Collins <munisco12@gmail.com>
2022-07-07 08:15:47 +0100

    added tradingview widget

--- src/pages/dashboard/team.tsx
+++ src/pages/dashboard/team.tsx
@@ -25,44 +25,10 @@ function team() {
     }, [theme])
 
     return (
-        <div className="h-[71vh]">
-
-        <div className='py-5 px-2 grid justify-center justify-items-center'>
-            <h1
-                style={neuToUse}
-                className=' justify-self-center text-2xl px-2 py-1 mt-4 mb-5'
-            >
-                My Team
-            </h1>
-            <div className='grid justify-items-center gap-4'>
-                <BsFillFilePersonFill className='text-4xl' />
-
-                <div className='grid'>
-                    <h3 className='text-xl'>
-                        Upline: <span className='font-semibold'>Tester1</span>
-                    </h3>
-                </div>
+        <div className='h-[71vh]'>
+            <div className='py-5 px-2 grid justify-center justify-items-center'>
+                
             </div>
-            <div className='grid mt-5 border border-gray-500 rounded-lg py-2 px-5'>
-                <div className='grid'>
-                    <p>
-                        Total Referral Commission
-                        <span className='font-semibold'>: $97</span>
-                    </p>
-                </div>
-                <div className='grid'>
-                    <p>
-                        Referrals<span className='font-semibold'>: 35</span>
-                    </p>
-                </div>
-                <div className='grid'>
-                    <p>
-                        Actival Referrals
-                        <span className='font-semibold'>: 27</span>
-                    </p>
-                </div>
-            </div>
-        </div>
         </div>
     )
 }
