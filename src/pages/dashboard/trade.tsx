df006b90da9d124e82fadfc9f327d1e41d5d0acd
Igboanugwo Collins <munisco12@gmail.com>
2022-07-07 07:25:28 +0100

    added tradingview widget

--- src/pages/dashboard/trade.tsx
+++ src/pages/dashboard/trade.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import AdvancedRealChart from '../../widgets/AdvancedRealChart'
-import TradeViewChart from 'react-crypto-chart'
 
 
 function trade() {
     return (
         <div className='h-[90vh]'>
             trade
-<TradeViewChart pair='BTCBUSD' i />
         </div>
     )
 }
