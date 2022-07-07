bc2e71ffe482ba47d778fb9502d76021b909f2f1
Igboanugwo Collins <munisco12@gmail.com>
2022-07-07 08:43:57 +0100

    working on team referral

--- src/pages/dashboard/team.tsx
+++ src/pages/dashboard/team.tsx
@@ -37,7 +37,7 @@ function team() {
                         <p className='font-bold'>$15</p>
                     </div>
                 </div>
-                <button className='text-center bg-blue-400 py-2 px-5'>Transfer</button>
+                <button className='text-center bg-blue-400 py-2 px-5 rounded-b-lg'>Transfer</button>
             </div>
         </div>
     )
