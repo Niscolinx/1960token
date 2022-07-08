90630a101a24531afd4bbdf627735831a5759a83
Igboanugwo Collins <munisco12@gmail.com>
2022-07-08 15:51:04 +0100

    working on widget

--- src/pages/dashboard/trade.tsx
+++ src/pages/dashboard/trade.tsx
@@ -1,24 +1,21 @@
 import axios from 'axios'
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Script from 'next/script'
+import TradingViewWidget, { Themes } from 'react-tradingview-widget'
+
 
 function trade() {
-    const [tradingView, setTradingView] = useState()
-    useEffect(() => {
-         axios.get('https://s3.tradingview.com/tv.js').then(({data}) => setTradingView(data)).catch(err => console.log(err))
-    }, [])
+    const [tradingView, setTradingView] = useState({})
 
+    // useEffect(() => {
+    //      axios.get('https://s3.tradingview.com/tv.js').then(({data}) => setTradingView(data)).catch(err => console.log(err))
+    // }, [])
 
-    useEffect(() => {
-        console.log({tradingView})
-    }, [tradingView])
-    
+    console.log('window', tradingView)
 
     return (
         <div className='h-[90vh]'>
-            <Script
-                src='https://s3.tradingview.com/tv.js'
-            ></Script>
+            <iframe src='/tradingView.html' width='100%'></iframe>
             Trade
         </div>
     )
