97b8d11459219d982dca7b94bf03da47bc32fdcd
Igboanugwo Collins <munisco12@gmail.com>
2022-07-08 13:50:36 +0100

    working on team referral

--- src/pages/dashboard/trade.tsx
+++ src/pages/dashboard/trade.tsx
@@ -6,7 +6,20 @@ function trade() {
     return (
         <div className='h-[90vh]'>
             trade
-      <CryptoMarket/>
+            <div className='tradingview-widget-container'>
+                <div id='tradingview_3800e' />
+                <div className='tradingview-widget-copyright'>
+                    <a
+                        href='https://www.tradingview.com/
+  symbols/BTCUSDT/?exchange=BINANCE'
+                        rel='noopener'
+                        target='_blank'
+                    >
+                        <span className='blue-text'>BTCUSDT Chart</span>
+                    </a>{' '}
+                    by TradingView
+                </div>
+            </div>{' '}
         </div>
     )
 }
