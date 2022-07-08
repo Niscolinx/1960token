2f9daeec39f375fd0352b4bbce57cd8839b9175d
Igboanugwo Collins <munisco12@gmail.com>
2022-07-08 15:10:14 +0100

    working on widget

--- src/pages/dashboard/trade.tsx
+++ src/pages/dashboard/trade.tsx
@@ -1,6 +1,13 @@
+import axios from 'axios'
 import React, {useEffect, useState} from 'react'
 
 function trade() {
+
+    useEffect(async() => {
+        const res = await axios.get('https://s3.tradingview.com/tv.js')
+        return res
+    }, [])
+
     return <div className='h-[90vh]'>Trade</div>
 }
 
