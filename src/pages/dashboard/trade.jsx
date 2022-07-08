57c666704f71fe587f58a3f1f1d8f7346064596a
Igboanugwo Collins <munisco12@gmail.com>
2022-07-08 14:47:35 +0100

    working on widget

---
+++ src/pages/dashboard/trade.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+//import AdvancedChart from '../../tradeviewWidgets/AdvancedRealChart'
+
+function trade() {
+    console.log('the window location', window.location)
+    return <div className='h-[90vh]'>Trade</div>
+}
+
+export default trade
