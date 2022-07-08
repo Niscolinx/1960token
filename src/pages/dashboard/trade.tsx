c059817d9c7679730286b2bbf1423f5299385d3a
Igboanugwo Collins <munisco12@gmail.com>
2022-07-08 22:57:12 +0100

    working on trading view

--- src/pages/dashboard/trade.tsx
+++ src/pages/dashboard/trade.tsx
@@ -28,7 +28,7 @@ function trade() {
     return (
         <div className='h-[90vh]'>
 
-            <iframe src='/tradingView.html' width='100%' height={'600px'}/>
+            <iframe src='/tradingView.html' width='100%' style={{height: '60vh'}}/>
         </div>
     )
 }
