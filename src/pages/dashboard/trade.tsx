641ed259e274cc5c81df16a1bd38e9710130bc3a
Igboanugwo Collins <munisco12@gmail.com>
2022-07-08 15:14:56 +0100

    working on widget

--- src/pages/dashboard/trade.tsx
+++ src/pages/dashboard/trade.tsx
@@ -4,8 +4,13 @@ import React, {useEffect, useState} from 'react'
 function trade() {
     const [tradingView, setTradingView] = useState()
     useEffect(() => {
-         axios.get('https://s3.tradingview.com/tv.js').then(data => console.log({data})).catch(err => console.log(err))
+         axios.get('https://s3.tradingview.com/tv.js').then(({data}) => setTradingView(data)).catch(err => console.log(err))
     }, [])
+
+
+    useEffect(() => {
+        console.log({tradingView})
+    }, [tradingView])
 
     return <div className='h-[90vh]'>Trade</div>
 }
