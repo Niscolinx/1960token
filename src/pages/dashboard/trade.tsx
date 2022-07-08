b0d92afbd6280c078f2ab22df3c53d4824b3b32f
Igboanugwo Collins <munisco12@gmail.com>
2022-07-08 15:12:45 +0100

    working on widget

--- src/pages/dashboard/trade.tsx
+++ src/pages/dashboard/trade.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, {useEffect, useState} from 'react'
 
 function trade() {
-
+    const [tradingView, setTradingView] = useState()
     useEffect(() => {
          axios.get('https://s3.tradingview.com/tv.js').then(data => console.log({data})).catch(err => console.log(err))
     }, [])
