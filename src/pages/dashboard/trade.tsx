dee4f01b4cb9cab42208fb76307467922bfa0001
Igboanugwo Collins <munisco12@gmail.com>
2022-07-08 17:44:34 +0100

    implemented the trading view widget

--- src/pages/dashboard/trade.tsx
+++ src/pages/dashboard/trade.tsx
@@ -1,7 +1,4 @@
-import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import Script from 'next/script'
-import TradingViewWidget, { Themes } from 'react-tradingview-widget'
 
 
 function trade() {
