ea8cc193d5e601b4e83ff645eeee711e8d5b2525
Igboanugwo Collins <munisco12@gmail.com>
2022-07-10 06:23:13 +0100

    working on referral

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -8,97 +8,92 @@ async function signupHandler(req: NextApiRequest, res: NextApiResponse) {
     try {
         await dbConnect()
 
-            console.log('req body', req.body)
-            const { email, password, username, phoneNumber, referral } = req.body
-            //Validate
-            if (!email || !email.includes('@') || !password || !phoneNumber) {
-                console.log('failed')
-                res.status(422).json({ message: 'Invalid Data' })
-                return
-            }
+        console.log('req body', req.body)
+        const { email, password, username, phoneNumber, referral } = req.body
+        //Validate
+        if (!email || !email.includes('@') || !password || !phoneNumber) {
+            console.log('failed')
+            res.status(422).json({ message: 'Invalid Data' })
+            return
+        }
 
-            const existingUserEmail = await User.findOne({ email })
-            const existingUsername = await User.findOne({
-                username,
+        const existingUserEmail = await User.findOne({ email })
+        const existingUsername = await User.findOne({
+            username,
+        })
+        const existingPhoneNumber = await User.findOne({ phoneNumber })
+
+        if (existingUserEmail) {
+            return res.status(401).json({
+                message: 'Email already exists',
             })
-            const existingPhoneNumber = await User.findOne({ phoneNumber })
-            
-            
-            
+        }
 
+        if (existingUsername) {
+            return res.status(401).json({
+                message: 'Username already taken',
+            })
+        }
 
-            if (existingUserEmail) {
-                 return res.status(401).json({
-                    message: 'Email already exists'
-                 })
-            }
+        if (existingPhoneNumber) {
+            return res.status(401).json({
+                message: 'Number already in use',
+            })
+        }
+        if (referral) {
+            const checkReferral = await User.findOne({ username: referral })
 
-            if (existingUsername) {
-                 return res.status(401).json({
-                     message: 'Username already taken',
-                 })
-            }
-
-            if (existingPhoneNumber) {
+            if (!checkReferral) {
                 return res.status(401).json({
-                    message: 'Number already in use',
+                    message: 'Referral not found',
                 })
-            }
-            // if (referral) {
-            //     const checkReferral = await User.findOne({ username: referral })
+            } else {
+                console.log('referral found')
+                const userReferral = new UserReferral({
+                    username,
+                    level: 1,
+                    referral: checkReferral
+                })
 
-            //     if (!checkReferral) {
-            //         return res.status(401).json({
-            //             message: 'Referral not found',
-            //         })
-            //     } else {
-            //         console.log('referral found')
-            //         const userReferral = new UserReferral({
-            //             username,
-            //             level: 1,
-            //             referral: checkReferral
-            //         })
+                const referredUser = await userReferral.save()
 
-            //         const referredUser = await userReferral.save()
+                console.log({referredUser})
 
-            //         console.log({referredUser})
+        // checkReferral.referral.push(referredUser)
 
-                    // checkReferral.referral.push(referredUser)
+        // await checkReferral.save()
 
-                    // await checkReferral.save()
+        // console.log({checkReferral})
 
-                    // console.log({checkReferral})
+        // if(checkReferral.upliner){
+        //     console.log("user upliner", checkReferral.upliner)
+        // }
+        // else{
+        //     console.log('no upliner')
+         }
+         }
+        // }
 
-                    // if(checkReferral.upliner){
-                    //     console.log("user upliner", checkReferral.upliner)
-                    // }
-                    // else{
-                    //     console.log('no upliner')
-                    // }
-               // }
-           // }
+        // const storeUser = new User({
+        //     email,
+        //     password: await bcrypt.hash(password, 12),
+        //     username,
+        //     phoneNumber,
+        //     referral
+        // })
 
-            // const storeUser = new User({
-            //     email,
-            //     password: await bcrypt.hash(password, 12),
-            //     username,
-            //     phoneNumber,
-            //     referral
-            // })
+        // const verifyStored = await storeUser.save()
 
-            // const verifyStored = await storeUser.save()
-
-            // if (verifyStored) {
-            //     res.status(201).json({
-            //         message: 'successful',
-            //     })
-            // }
-            // else{
-            //     res.status(404).json({
-            //         message: 'failed'
-            //     })
-            // }
-        
+        // if (verifyStored) {
+        //     res.status(201).json({
+        //         message: 'successful',
+        //     })
+        // }
+        // else{
+        //     res.status(404).json({
+        //         message: 'failed'
+        //     })
+        // }
     } catch (err) {
         console.log({ err })
     }
