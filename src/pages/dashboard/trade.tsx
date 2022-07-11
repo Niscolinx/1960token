ada431c9d0e74f7d518bed034e304b9f4bd81242
Igboanugwo Collins <munisco12@gmail.com>
2022-07-11 06:16:01 +0100

    working on referral

--- src/pages/dashboard/trade.tsx
+++ src/pages/dashboard/trade.tsx
@@ -3,34 +3,31 @@ import { useRouter } from 'next/router'
 
 function trade() {
     const router = useRouter()
-    
-    useEffect(() => {
-        return router.reload()
-    }, [])
 
+ 
 
-    useEffect(() => {
-        Array.from(document.getElementsByTagName('iframe')).forEach(
-            (iframe) => {
-                iframe.contentWindow!.addEventListener(
-                    'load',
-                    () => {
-                        const doc = iframe.contentWindow!.document
-                        iframe.height = doc.body.scrollHeight.toString()
-                    },
-                    true
-                )
-                iframe.contentWindow!.addEventListener(
-                    'resize',
-                    () => {
-                        iframe.height =
-                            iframe.contentWindow!.document.body.scrollHeight.toString()
-                    },
-                    true
-                )
-            }
-        )
-    }, [])
+    // useEffect(() => {
+    //     Array.from(document.getElementsByTagName('iframe')).forEach(
+    //         (iframe) => {
+    //             iframe.contentWindow!.addEventListener(
+    //                 'load',
+    //                 () => {
+    //                     const doc = iframe.contentWindow!.document
+    //                     iframe.height = doc.body.scrollHeight.toString()
+    //                 },
+    //                 true
+    //             )
+    //             iframe.contentWindow!.addEventListener(
+    //                 'resize',
+    //                 () => {
+    //                     iframe.height =
+    //                         iframe.contentWindow!.document.body.scrollHeight.toString()
+    //                 },
+    //                 true
+    //             )
+    //         }
+    //     )
+    // }, [])
 
     return (
         <div className='h-[90vh]'>
