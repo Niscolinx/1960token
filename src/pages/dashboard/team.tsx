6ef0f1554a08b8433f3e31b22b91735892efdb54
Igboanugwo Collins <munisco12@gmail.com>
2022-07-11 10:11:42 +0100

    working on referral

--- src/pages/dashboard/team.tsx
+++ src/pages/dashboard/team.tsx
@@ -112,7 +112,7 @@ function team() {
         toggleDisplay(display ? false : true)
     }
 
-    const toDisplay =    {
+    const toDisplay = {
         opacity: '0',
         height: '0',
         visibility: 'hidden'
@@ -133,7 +133,7 @@ function team() {
                         <p className='font-bold'>${fetchedUser.totalMined}</p>
                     </div>
                 </div>
-                <div className='grid' style={!display ? toDisplay: {}}>
+                <div className='grid' style={toDisplay}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Tenetur nam velit dolorem non voluptate provident quos, sit
                     nemo maxime quasi autem debitis harum. Quisquam quod laborum
