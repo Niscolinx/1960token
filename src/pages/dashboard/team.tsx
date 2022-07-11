f592ce9f9d39819ba586431cca38043d68485d0a
Igboanugwo Collins <munisco12@gmail.com>
2022-07-11 15:57:26 +0100

    working on transition

--- src/pages/dashboard/team.tsx
+++ src/pages/dashboard/team.tsx
@@ -146,7 +146,7 @@ function team() {
                               }
                     }
                 >
-                    <form>
+                    <form className='px-10 py-12 grid'>
                         <select
                             className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-white `}
                         >
