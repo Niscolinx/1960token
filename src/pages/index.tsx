735172820d3f34af364334dcd66fb78aa659aa95
Igboanugwo Collins <munisco12@gmail.com>
2022-07-12 10:27:19 +0100

    working on dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,7 +59,10 @@ const Index = () => {
     return (
         <>
             <div className='grid gap-5 max-w-5xl mx-auto text-gray-400 px-4'>
-                hello
+                <div className='flex'>
+                    <Logo className='text-blue-400 text-3xl light:text-[#1a1a2d] w-12 h-12' />
+                    1960Token
+                </div>
                 <Swiper
                     spaceBetween={30}
                     centeredSlides={true}
