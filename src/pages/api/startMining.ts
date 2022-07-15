a331ce793712d470d5f450ca4d6b6a28d7fd7b24
Igboanugwo Collins <munisco12@gmail.com>
2022-07-15 10:41:10 +0100

    working on videoAd

--- src/pages/api/startMining.ts
+++ src/pages/api/startMining.ts
@@ -18,7 +18,7 @@ export default async function StartMining(
     req: NextApiRequest,
     res: TypedReqBody<returnTypeJson>
 ) {
-    const startTimeStamp = dayjs().add(1, 'minute')
+    const startTimeStamp = dayjs().add(12, 'hour')
 
     const { email }: IUser = req.body.user
 
