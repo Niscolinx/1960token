9be6060cae8077797120a1911662326a1a377e97
Igboanugwo Collins <munisco12@gmail.com>
2022-07-15 18:40:34 +0100

    working on portfolio

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -3,10 +3,10 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import User from '../../models/User'
 
 export default async function user(req: NextApiRequest, res: NextApiResponse) {
-    console.log('req body', req.body)
     const { email }: IUser = req.body.user
 
     const user = await User.findOne({ email })
+
 
     return res.json(user)
 }
