0dd2680db272a749d950ff9d0a91960cf4ace791
Igboanugwo Collins <munisco12@gmail.com>
2022-07-20 13:20:05 +0100

    working on profile page

--- src/pages/dashboard/profile.tsx
+++ src/pages/dashboard/profile.tsx
@@ -2,7 +2,155 @@ import { GetSessionParams, getSession } from 'next-auth/react'
 import React from 'react'
 
 function profile() {
-    return <div className='px-4 h-[80vh]'>profile</div>
+    return (
+        <div className='px-4 h-[80vh]'>
+            <form
+                id='register'
+                className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 grid m-2'
+                onSubmit={handleSubmit}
+            >
+                <div className='mb-4'>
+                    <p
+                        className={`${messageDisplay} ${message?.style} text-sm text-center mb-5`}
+                    >
+                        {message?.value}
+                    </p>
+                    <label
+                        className='block text-gray-700 text-sm font-bold mb-2'
+                        htmlFor='username'
+                    >
+                        Username
+                    </label>
+                    <input
+                        className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-white ${
+                            error && errorFields.includes('username')
+                                ? 'border-red-500'
+                                : ''
+                        }`}
+                        id='username'
+                        name='username'
+                        required
+                        type='text'
+                        minLength={4}
+                        value={username}
+                        onChange={changeHandler}
+                    />
+                </div>
+
+                <div className='mb-4'>
+                    <label
+                        className='block text-gray-700 text-sm font-bold mb-2'
+                        htmlFor='phoneNumber'
+                    >
+                        Phone No
+                    </label>
+                    <input
+                        className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-white ${
+                            error && errorFields.includes('phoneNumber')
+                                ? 'border-red-500'
+                                : ''
+                        }`}
+                        id='phoneNumber'
+                        type='number'
+                        name='phoneNumber'
+                        required
+                        value={phoneNumber}
+                        onChange={changeHandler}
+                    />
+                </div>
+                <div className='mb-4'>
+                    <label
+                        className='block text-gray-700 text-sm font-bold mb-2'
+                        htmlFor='username'
+                    >
+                        Email
+                    </label>
+                    <input
+                        className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-white ${
+                            error && errorFields.includes('email')
+                                ? 'border-red-500'
+                                : ''
+                        }`}
+                        id='email'
+                        type='email'
+                        name='email'
+                        required
+                        value={email}
+                        onChange={changeHandler}
+                    />
+                </div>
+                <div className='mb-6'>
+                    <label
+                        className='block text-gray-700 text-sm font-bold mb-2'
+                        htmlFor='password'
+                    >
+                        Password
+                    </label>
+                    <input
+                        className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-white ${
+                            error && errorFields.includes('password')
+                                ? 'border-red-500'
+                                : ''
+                        }`}
+                        id='password'
+                        name='password'
+                        type='password'
+                        minLength={6}
+                        required
+                        value={password}
+                        onChange={changeHandler}
+                    />
+                </div>
+                <div className='mb-6'>
+                    <label
+                        className='block text-gray-700 text-sm font-bold mb-2'
+                        htmlFor='confirmPassword'
+                    >
+                        Confirm Password
+                    </label>
+                    <input
+                        className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-white ${
+                            error && errorFields.includes('confirmPassword')
+                                ? 'border-red-500'
+                                : ''
+                        }`}
+                        id='confirmPassword'
+                        name='confirmPassword'
+                        type='password'
+                        minLength={6}
+                        required
+                        value={confirmPassword}
+                        onChange={changeHandler}
+                    />
+                </div>
+                <div className='mb-6'>
+                    <label
+                        className='block text-gray-700 text-sm font-bold mb-2'
+                        htmlFor='referral'
+                    >
+                        Referral
+                    </label>
+                    <input
+                        className={`shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-white`}
+                        id='referral'
+                        name='referral'
+                        type='text'
+                        placeholder='optional'
+                        minLength={3}
+                        value={referral}
+                        onChange={changeHandler}
+                    />
+                </div>
+
+                <button
+                    className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline  justify-self-center'
+                    type='submit'
+                >
+                    {loading ? 'Loading...' : 'Register'}
+                </button>
+            </form>
+        </div>
+    )
 }
 
 export default profile
