9ffa788ba16b565ff3771db8fe973cd3855cf942
Igboanugwo Collins <munisco12@gmail.com>
2022-07-20 07:23:37 +0100

    working on referral link

--- src/models/User.ts
+++ src/models/User.ts
@@ -17,6 +17,7 @@ export interface IUser {
     status: string
     password: string
     referrals: any[]
+    referralLink: string
     pendingWithdrawals: any[]
     pendingDeposits: any[]
     totalWithdrawals: any[]
