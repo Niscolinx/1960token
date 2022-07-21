7d66a21c87e110e1f4673da32959a7c8e6983ad0
Igboanugwo Collins <munisco12@gmail.com>
2022-07-21 16:38:58 +0100

    created the table

--- src/pages/dashboard/transactionHistory.tsx
+++ src/pages/dashboard/transactionHistory.tsx
@@ -28,6 +28,8 @@ type Props = TransactionData[]
 const transactionHistory = () => {
     const result = generateData()
 
+    
+
     return (
         <div className=' overflow-scroll overflow-visible'>
             <table className='border border-gray-700 transaction-table mx-5 md:mx-auto'>
