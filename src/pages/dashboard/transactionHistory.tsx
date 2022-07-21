b4c247aff121c83ac70832d18defda91b8f3c012
Igboanugwo Collins <munisco12@gmail.com>
2022-07-21 09:42:25 +0100

    working on transaction history

--- src/pages/dashboard/transactionHistory.tsx
+++ src/pages/dashboard/transactionHistory.tsx
@@ -1,46 +1,43 @@
 import react from 'react'
 
-const generateData = ():Props => {
-
-    let filledArray = new Array(10)
-    for (let i = 0; i < filledArray.length; i++) {
-        const date = new Date().toLocaleString('en-GB', {month: 'short', day: '2-digit', year: 'numeric'})
-        filledArray[i] = {
-            date,
-            type: Math.random() < 0.5 ? 'Transfer' : 'Withdrawn',
-            amount: Math.floor(Math.random() * (300 - 10 + 1) + 10),
-        }
-    }
+const generateData = (): Props => {
+    const date = new Date().toLocaleString('en-GB', {
+        month: 'short',
+        day: '2-digit',
+        year: 'numeric',
+    })
+    let filledArray = Array.from({ length: 40 }, () => ({
+        date,
+        type: Math.random() < 0.5 ? 'Transfer' : 'Withdrawn',
+        amount: Math.floor(Math.random() * (300 - 10 + 1) + 10),
+    }))
 
     return filledArray
 }
 
 type TransactionData = {
-    date: Date
+    date: string
     type: string
     amount: number
 }
 
-type Props =  TransactionData[]
-
-
-
+type Props = TransactionData[]
 
 const transactionHistory = () => {
     const result = generateData()
 
     return (
-        <>
+        <div className='flex flex-wrap'>
             {result.map((value, i) => {
                 return (
-                    <div key={i}>
+                    <div key={i} className='flex'>
                         <p>{value.date}</p>
                         <p>{value.type}</p>
                         <p>{value.amount}</p>
                     </div>
                 )
             })}
-        </>
+        </div>
     )
 }
 
