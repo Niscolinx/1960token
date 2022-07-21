d6ed07b678f799307f61aff286b5b08df87f7e3e
Igboanugwo Collins <munisco12@gmail.com>
2022-07-21 16:56:03 +0100

    created the table

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,5 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient } from 'mongodb'
-import { Connection, ConnectOptions } from 'mongoose'
 
 const uri = process.env.MONGODB_URI || ''
 const options = {
@@ -16,9 +15,7 @@ if (!process.env.MONGODB_URI) {
 }
 
 if (process.env.NODE_ENV === 'development') {
-    // In development mode, use a global variable so that the value
-    // is preserved across module reloads caused by HMR (Hot Module Replacement).
-
+  
 
     if (!global._mongoClientPromise) {
         client = new MongoClient(uri, options as any)
@@ -26,7 +23,6 @@ if (process.env.NODE_ENV === 'development') {
     }
     clientPromise = global._mongoClientPromise
 } else {
-    // In production mode, it's best to not use a global variable.
     client = new MongoClient(uri, options as any)
     clientPromise = client.connect()
 }
