17782fac510935bd409eba8231fd3ee30edcfa2c
Igboanugwo Collins <munisco12@gmail.com>
2022-07-21 18:25:20 +0100

    created the table

--- src/pages/api/transactions.ts
+++ src/pages/api/transactions.ts
@@ -3,14 +3,18 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import User from '../../models/User'
 import transaction from '../../models/transaction'
 
-export default async function transactions(req: NextApiRequest, res: NextApiResponse) {
+export default async function transactions(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
     const { email }: IUser = req.body.user
 
     const user = await User.findOne({ email })
 
+    if (!user) {
+        return res.status(404).json('User not found')
+    }
     const transactions = await transaction.find({ creator: user._id })
-
-
 
     return res.json(transactions)
 }
