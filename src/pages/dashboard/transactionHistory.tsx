8ddc5ce357544ca7c31ea54d95bf7491d5445a17
Igboanugwo Collins <munisco12@gmail.com>
2022-07-21 19:04:58 +0100

    created the table

--- src/pages/dashboard/transactionHistory.tsx
+++ src/pages/dashboard/transactionHistory.tsx
@@ -14,7 +14,6 @@ const transactionHistory = () => {
     const [email, setEmail] = useState('')
     const [transactionData, setTransactionData] = useState<Props>([])
 
-
     useEffect(() => {
         const userJSON = localStorage.getItem('userSession')
 
@@ -22,8 +21,6 @@ const transactionHistory = () => {
             const userData = JSON.parse(userJSON)
 
             const { email } = userData
-
-            
 
             setEmail(email)
         }
@@ -36,10 +33,12 @@ const transactionHistory = () => {
         const fetch = async () => {
             const res = await axios.post('/api/transactions', { user })
 
-            const updatedResponse = res.data.map((data:TransactionData) => {
+            const updatedResponse = res.data.map((data: TransactionData) => {
                 return {
                     ...data,
-                    createdAt: new Date(data.createdAt).toLocaleDateString('en-GB')
+                    createdAt: new Date(data.createdAt).toLocaleDateString(
+                        'en-GB'
+                    ),
                 }
             })
 
@@ -53,39 +52,37 @@ const transactionHistory = () => {
 
     return (
         <div className='min-h-[80vh]'>
-
-            <div className="grid">
-
-            <table className='border border-gray-700 transaction-table mx-5 md:mx-auto'>
-                <caption className='font-semibold text-lg py-5'>
-                    Transaction History
-                </caption>
-                <colgroup>
-                    <col className='w-40 min-w-30' span={5} />
-                </colgroup>
-                <thead>
-                    <tr className='mb-10'>
-                        <th rowSpan={4}>No</th>
-                        <th>Date</th>
-                        <th>Type</th>
-                        <th>Amount</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody className='text-center'>
-                    {transactionData.map((value, i) => {
-                        return (
-                            <tr key={i} className='mx-4 py-2'>
-                                <td>{i + 1}</td>
-                                <td>{value.createdAt}</td>
-                                <td>{value.type}</td>
-                                <td>{value.amount}</td>
-                                <td>{value.status}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+            <div className='grid overflow-scroll max-h-[70vh]'>
+                <table className='border border-gray-700 transaction-table mx-5 md:mx-auto'>
+                    <caption className='font-semibold text-lg py-5'>
+                        Transaction History
+                    </caption>
+                    <colgroup>
+                        <col className='w-40 min-w-30' span={5} />
+                    </colgroup>
+                    <thead>
+                        <tr className='mb-10'>
+                            <th rowSpan={4}>No</th>
+                            <th>Date</th>
+                            <th>Type</th>
+                            <th>Amount</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody className='text-center'>
+                        {transactionData.map((value, i) => {
+                            return (
+                                <tr key={i} className='mx-4 py-2'>
+                                    <td>{i + 1}</td>
+                                    <td>{value.createdAt}</td>
+                                    <td>{value.type}</td>
+                                    <td>{value.amount}</td>
+                                    <td>{value.status}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
             </div>
         </div>
     )
