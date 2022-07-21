358bfe74faa753f5306f9a9fd4ef989261dcdd21
Igboanugwo Collins <munisco12@gmail.com>
2022-07-21 07:38:39 +0100

    working on transaction history

--- src/pages/dashboard/transactionHistory.tsx
+++ src/pages/dashboard/transactionHistory.tsx
@@ -1,8 +1,24 @@
 import react from 'react'
 
-const transactionHistory = () => {
-    return <>Transaction History</>
-}
+const data = [{ date: Date.now() }]
 
+const transactionHistory = () => {
+    let filledArray = new Array(10)
+    for (let i = 0; i < filledArray.length; i++) {
+        filledArray[i] = { hello: 'goodbye' }
+    }
+    //let filledArray = new Array(10).fill(null).map(() => ({ hello: 'goodbye' }))
+
+    filledArray[5].hello = 'bye'
+
+    console.log({ filledArray })
+    return (
+        <>
+            {filledArray.map((value) => {
+                return <p>{value.hello}</p>
+            })}
+        </>
+    )
+}
 
 export default transactionHistory
