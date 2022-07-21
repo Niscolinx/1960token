0db189a93a3b7e89fef408e954ed8c1af26e0f81
Igboanugwo Collins <munisco12@gmail.com>
2022-07-21 17:47:19 +0100

    created the table

---
+++ src/pages/api/transactions.ts
@@ -0,0 +1,15 @@
+import { IUser } from '../../models/User'
+import { NextApiRequest, NextApiResponse } from 'next'
+import User from '../../models/User'
+import transaction from '../../models/transaction'
+
+export default async function transactions(req: NextApiRequest, res: NextApiResponse) {
+    const { email }: IUser = req.body.user
+
+    const user = await User.findOne({ email })
+
+   console.log(user.transactions)
+
+
+    return res.json(user)
+}
