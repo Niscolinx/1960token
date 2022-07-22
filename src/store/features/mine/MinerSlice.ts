3d22c1bcd6ec03a21e8e525ba70a2393539849ed
Igboanugwo Collins <munisco12@gmail.com>
2022-07-22 06:58:05 +0100

    fixing a bug in mining

--- src/store/features/mine/MinerSlice.ts
+++ src/store/features/mine/MinerSlice.ts
@@ -25,11 +25,6 @@ const initialState: MiningState = {
     tokensCount: 0
 }
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
 export const initMineAsync = createAsyncThunk(
     'mining',
     async (userSession: nextAuthSession | any) => {
