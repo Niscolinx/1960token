14b437150e67662be9e1d7d5b8d5bf9954af57da
Igboanugwo Collins <munisco12@gmail.com>
2022-07-26 13:54:17 +0100

    working on terms and conditions

--- src/pages/terms-and-conditions.tsx
+++ src/pages/terms-and-conditions.tsx
@@ -9,121 +9,118 @@ function TermsAndConditions() {
                 animateIn='animate__slideInDown'
             >
                 <h1 className='text-center px-2 py-5 text-3xl font-semibold mb-5 uppercase '>
-                    Privacy Policy
+                    Terms and Conditions
                 </h1>
+                <p className='text-sm'>1960Token Terms and Conditions of Use</p>
             </AnimationOnScroll>
             <div className='grid'>
-                <AnimationOnScroll
-                    animateOnce={true}
-                    animateIn='animate__slideInDown'
-                >
-                    <h2 className='text-center font-semibold text-2xl grad p-2 uppercase uppercase'>
-                        Security and privacy
-                    </h2>
-                </AnimationOnScroll>
-                <p className='text-justify'>
-                    The Company recognises the importance of protecting the
-                    client's personal and financial information. All the
-                    information that the Company obtains about the client
-                    assists the Company in servicing the client and the client's
-                    account. The Company knows that the client may be concerned
-                    about what the Company does with this information
-                </p>
-            </div>
-
-            <div className='grid'>
-                <AnimationOnScroll
-                    animateOnce={true}
-                    animateIn='animate__slideInDown'
-                >
-                    <h2 className='text-center font-semibold text-2xl grad p-2 uppercase'>
-                        Cookies{' '}
-                    </h2>
-                </AnimationOnScroll>
-                <p className='text-justify'>
-                    Cookies are small text files stored on computer drives and
-                    are widely used in order to make websites work and to
-                    improve the user experience. All recent versions of browsers
-                    give the client a level of control over cookies. The client
-                    can delete all cookies that are already on the client's
-                    computer, and the browser can be set to prevent them from
-                    being placed. However, if the client chooses not to receive
-                    the Company's cookies, the full usability of the Company's
-                    website may be adversely affected.
-                </p>
-                <p className='text-justify'>
-                    The client should note that the Company's website generates
-                    log files that record the IP addresses of accesses to the
-                    client's account, login attempts, and device information
-                    such as the manufacturer, model, operating system, and
-                    browser. This information is gathered for the sole purpose
-                    of providing assistance with investigating a client's
-                    account in the unlikely event that the account is accessed
-                    by unauthorised users. Information supplied by some cookies
-                    also helps the Company understand how visitors use the
-                    website, so that the Company can improve how it presents its
-                    content.
-                </p>
-                <p className='text-justify'>
-                    The Company's cookies are not deemed dangerous and cannot
-                    access any other information on the client's computer.
-                </p>
-                <p className='text-justify'>
-                    To provide the client with a better experience, some of the
-                    services offered by Binary.com may require permission to
-                    access the client's cloud storage services, such as Google
-                    Drive. In such instances
-                </p>
-                <ul className='list-decimal list-inside'>
-                    <li>
-                        The Company does not store any data related to the
-                        client's cloud storage service on any of the Company's
-                        servers. All files are downloaded on the client's local
-                        machines.{' '}
-                    </li>
-                    <li>
-                        The Company does not share any data related to the
-                        client's cloud storage service with anyone
-                    </li>
-                    <li>
-                        The Company only accesses the client's cloud storage
-                        when the client's action initiates it. Clients can
-                        disconnect their storage service at any time
-                    </li>
-                </ul>
-            </div>
-
-            <div className='grid'>
-                <AnimationOnScroll
-                    animateOnce={true}
-                    animateIn='animate__slideInDown'
-                >
-                    <h2 className='text-center font-semibold text-2xl grad p-2'>
-                        Notification of Changes
-                    </h2>
-                </AnimationOnScroll>
-                <AnimationOnScroll
-                    animateOnce={true}
-                    animateIn='animate__zoomInLeft'
-                >
-                    <>
-                        <p className='text-justify'>
-                            Any changes in the Company's privacy policy or
-                            security statement will be posted on this website.
-                            For any material changes that directly affect the
-                            economic use of the client's personal information,
-                            the Company will request the client's prior
-                            authorisation in writing before effecting such
-                            changes on the client's account.
-                        </p>
-                        <p className='text-justify'>
-                            The client also has the right to request that the
-                            Company inform the client about the personal data
-                            that the Company processes about the client and to
-                            provide its correction where necessary.
-                        </p>
-                    </>
-                </AnimationOnScroll>
+                <ol className='list-decimal list-inside'>
+                    <AnimationOnScroll
+                        animateOnce={true}
+                        animateIn='animate__slideInDown'
+                    >
+                        <li>
+                            By accessing this Website, accessible from
+                            https://www.1960token.com/, you are agreeing to be
+                            bound by these Website Terms and Conditions of Use
+                            and agree that you are responsible for the agreement
+                            with any applicable local laws. If you disagree with
+                            any of these terms, you are prohibited from
+                            accessing this site. The materials contained in this
+                            Website are protected by copyright and trade mark
+                            law.
+                        </li>
+                    </AnimationOnScroll>
+                    <AnimationOnScroll
+                        animateOnce={true}
+                        animateIn='animate__slideInDown'
+                    >
+                        <li>
+                            1960Token has not reviewed all of the sites linked
+                            to its Website and is not responsible for the
+                            contents of any such linked site. The presence of
+                            any link does not imply endorsement by 1960Token of
+                            the site. The use of any linked website is at the
+                            user’s own risk.
+                        </li>
+                    </AnimationOnScroll>
+                    <AnimationOnScroll
+                        animateOnce={true}
+                        animateIn='animate__slideInDown'
+                    >
+                        <li>
+                             1960Token may revise these Terms of Use for its
+                            Website at any time without prior notice. By using
+                            this Website, you are agreeing to be bound by the
+                            current version of these Terms and Conditions of
+                            Use.
+                        </li>
+                    </AnimationOnScroll>
+                    <AnimationOnScroll
+                        animateOnce={true}
+                        animateIn='animate__slideInDown'
+                    >
+                        <li>
+                            By accessing this Website, accessible from
+                            https://www.1960token.com/, you are agreeing to be
+                            bound by these Website Terms and Conditions of Use
+                            and agree that you are responsible for the agreement
+                            with any applicable local laws. If you disagree with
+                            any of these terms, you are prohibited from
+                            accessing this site. The materials contained in this
+                            Website are protected by copyright and trade mark
+                            law.
+                        </li>
+                    </AnimationOnScroll>
+                    <AnimationOnScroll
+                        animateOnce={true}
+                        animateIn='animate__slideInDown'
+                    >
+                        <li>
+                            By accessing this Website, accessible from
+                            https://www.1960token.com/, you are agreeing to be
+                            bound by these Website Terms and Conditions of Use
+                            and agree that you are responsible for the agreement
+                            with any applicable local laws. If you disagree with
+                            any of these terms, you are prohibited from
+                            accessing this site. The materials contained in this
+                            Website are protected by copyright and trade mark
+                            law.
+                        </li>
+                    </AnimationOnScroll>
+                    <AnimationOnScroll
+                        animateOnce={true}
+                        animateIn='animate__slideInDown'
+                    >
+                        <li>
+                            By accessing this Website, accessible from
+                            https://www.1960token.com/, you are agreeing to be
+                            bound by these Website Terms and Conditions of Use
+                            and agree that you are responsible for the agreement
+                            with any applicable local laws. If you disagree with
+                            any of these terms, you are prohibited from
+                            accessing this site. The materials contained in this
+                            Website are protected by copyright and trade mark
+                            law.
+                        </li>
+                    </AnimationOnScroll>
+                    <AnimationOnScroll
+                        animateOnce={true}
+                        animateIn='animate__slideInDown'
+                    >
+                        <li>
+                            By accessing this Website, accessible from
+                            https://www.1960token.com/, you are agreeing to be
+                            bound by these Website Terms and Conditions of Use
+                            and agree that you are responsible for the agreement
+                            with any applicable local laws. If you disagree with
+                            any of these terms, you are prohibited from
+                            accessing this site. The materials contained in this
+                            Website are protected by copyright and trade mark
+                            law.
+                        </li>
+                    </AnimationOnScroll>
+                </ol>
             </div>
         </div>
     )
