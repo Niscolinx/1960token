6ee0fd40544411f17e7e5063d93ddc17b8fab402
Igboanugwo Collins <munisco12@gmail.com>
2022-07-26 14:47:45 +0100

    working on terms and conditions

--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -163,13 +163,44 @@ function FAQ() {
                         animateIn='animate__slideInDown'
                     >
                         <h4 className='font-semibold text-xl capitalize'>
-                            3. Can I open more than one account?
+                            3. How long does it take to process deposits and
+                            withdrawals?
                         </h4>
                         <p>
-                            You can open one account (in the fiat currency of
-                            your choice) with your email address or via your
-                            Apple, or Google logins. You can also add
-                            cryptocurrencies to your profile.
+                            The deposits and withdrawals will be processed
+                            within 1 day. However, your bank or money transfer
+                            service may require additional time to process your
+                            request.
+                        </p>
+                    </AnimationOnScroll>
+                </div>
+                <div className='grid'>
+                    <AnimationOnScroll
+                        animateOnce={true}
+                        animateIn='animate__slideInDown'
+                    >
+                        <h4 className='font-semibold text-xl capitalize'>
+                            4. My withdrawal verification link has expired. What
+                            do I do?
+                        </h4>
+                        <p>
+                            You can get a new link on the Cashier page. Click
+                            Withdraw and we'll email you a new link; please
+                            remember to use it within 1 hour.
+                        </p>
+                    </AnimationOnScroll>
+                </div>
+                <div className='grid'>
+                    <AnimationOnScroll
+                        animateOnce={true}
+                        animateIn='animate__slideInDown'
+                    >
+                        <h4 className='font-semibold text-xl capitalize'>
+                            5. How can I cancel my withdrawal?
+                        </h4>
+                        <p>
+                            Here you can cancel your withdrawal, By not
+                            accepting the email confirmation sent to your Email
                         </p>
                     </AnimationOnScroll>
                 </div>
@@ -180,7 +211,7 @@ function FAQ() {
                     animateIn='animate__slideInDown'
                 >
                     <h2 className='text-center font-semibold text-2xl grad p-2 uppercase'>
-                        General
+                        CryptoCurrency
                     </h2>
                 </AnimationOnScroll>
                 <div className='grid'>
@@ -189,11 +220,13 @@ function FAQ() {
                         animateIn='animate__slideInDown'
                     >
                         <h4 className='font-semibold text-xl capitalize'>
-                            1. Can I change the time zone on your website?
+                            1. How do I create a cryptocurrency account?{' '}
                         </h4>
                         <p>
-                            No, it can't be changed. However, your local time is
-                            shown when you hover over the time on our website:{' '}
+                            Follow the steps in this VIDEO to create a
+                            cryptocurrency account. It uses USDT as an example,
+                            but the process is the same for all cryptocurrency
+                            accounts.
                         </p>
                     </AnimationOnScroll>
                 </div>
@@ -203,25 +236,51 @@ function FAQ() {
                         animateIn='animate__slideInDown'
                     >
                         <h4 className='font-semibold text-xl capitalize'>
-                            2. Can I change my email address?
+                            2.What is a blockchain?
                         </h4>
-                        <p>No, Message us via our contact form, to know more</p>
+                        <p>
+                            It's a public ledger that records all the
+                            transactions taking place on a cryptocurrency
+                            network. A transaction that is performed on the
+                            cryptocurrency network is considered to be secured
+                            and irreversible once the blockchain processes and
+                            confirms the transaction.{' '}
+                        </p>
                     </AnimationOnScroll>
                 </div>
+            </div>
+            <div className='grid gap-4'>
+                <AnimationOnScroll
+                    animateOnce={true}
+                    animateIn='animate__slideInDown'
+                >
+                    <h2 className='text-center font-semibold text-2xl grad p-2 uppercase'>
+                        Affiliate Program{' '}
+                    </h2>
+                </AnimationOnScroll>
                 <div className='grid'>
                     <AnimationOnScroll
                         animateOnce={true}
                         animateIn='animate__slideInDown'
                     >
-                        <h4 className='font-semibold text-xl capitalize'>
-                            3. Can I open more than one account?
-                        </h4>
-                        <p>
-                            You can open one account (in the fiat currency of
-                            your choice) with your email address or via your
-                            Apple, or Google logins. You can also add
-                            cryptocurrencies to your profile.
-                        </p>
+                        <p className='text-xm'>When you join our affiliate programme</p>
+                        <ul className='list-decimal list-inside mt-5 grid gap-4'>
+                            <li>
+                                You'll have multiple income opportunities by
+                                joining other partnership programmes that we
+                                offer.
+                            </li>
+                            <li>
+                                You'll have access to various marketing tools to
+                                help promote our products and services to your
+                                clients.
+                            </li>
+                            <li>
+                                You'll be able to take advantage of our
+                                multilingual platforms and reach clients
+                                wherever they are
+                            </li>
+                        </ul>
                     </AnimationOnScroll>
                 </div>
             </div>
