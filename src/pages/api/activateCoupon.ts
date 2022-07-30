deec9ae1d1d83e1bc505ebc504e8084fef51e03e
Igboanugwo Collins <munisco12@gmail.com>
2022-07-30 14:35:42 +0100

    working on code check animation

--- src/pages/api/activateCoupon.ts
+++ src/pages/api/activateCoupon.ts
@@ -26,20 +26,21 @@ export default async function activateCoupon(
             return res.status(401).json('Code already used')
         }
 
-        code.isUsed = true
-        await code.save()
+        if (!isCheck) {
+            code.isUsed = true
 
-        const getUser = await User.findOne({email: user.email})
+            await code.save()
 
-        
-        getUser.isVerified = true
-        
-        await getUser.save()
-        
-        console.log({getUser})
-        
-        return res.status(200).json('Successfully activated')
+            const getUser = await User.findOne({ email: user.email })
 
+            getUser.isVerified = true
+
+            await getUser.save()
+
+            return res.status(200).json('Successfully activated')
+        } else {
+            return res.status(200).json('valid coupon')
+        }
     } catch (err) {
         console.log({ err })
         res.status(400).json('error')
